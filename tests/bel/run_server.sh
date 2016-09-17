6c4930e417273e2704541d9d4d50be90e11183b0
Charles Tapley Hoyt <cthoyt@gmail.com>
2016-09-17 20:52:22 +0200

    framework for final api testing
    - added test suite for from_url
    - added server script to run local server for bel files in test dir
    - added example BEL files

---
+++ tests/bel/run_server.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cd $PYBEL_BASE/tests/bel
+python -m SimpleHTTPServer
---
+++ tests/test_import.py
@@ -0,0 +1,47 @@
+import unittest
+
+import networkx as nx
+
+import pybel
+
+
+class TestImport(unittest.TestCase):
+    def setUp(self):
+        self.graph = nx.MultiGraph()
+
+        self.graph.add_node('TestValue1', namespace='TestNS1')
+        self.graph.add_node('TestValue2', namespace='TestNS1')
+        self.graph.add_node('TestValue3', namespace='TestNS1')
+        self.graph.add_node('TestValue4', namespace='TestNS1')
+        self.graph.add_node('TestValue5', namespace='TestNS1')
+
+        self.graph.add_edge('TestValue1', 'TestValue2', attr_dict={
+            'Citation': ("Pubmed", "That one article from last week", "123455"),
+            'Evidence': "Evidence 1 w extra notes",
+            'TESTAN1': 'TestAnnot1',
+            'relation': 'increases'
+        })
+        self.graph.add_edge('TestValue2', 'TestValue3', attr_dict={
+            'Citation': ("Pubmed", "That one article from last week", "123455"),
+            'Evidence': "Evidence 1 w extra notes",
+            'TESTAN1': 'TestAnnot1',
+            'relation': 'decreases',
+            'TESTAN2': 'B'
+        })
+        self.graph.add_edge('TestValue2', 'TestValue4', attr_dict={
+            'Citation': ("Pubmed", "That one article from last week", "123455"),
+            'Evidence': "Evidence 1 w extra notes",
+            'TESTAN1': 'TestAnnot1',
+            'relation': 'directlyDecreases',
+            'TESTAN2': 'B'
+        })
+        self.graph.add_edge('TestValue4', 'TestValue5', attr_dict={
+            'Citation': ("Pubmed", "That other article from last week", "123456"),
+            'Evidence': "Evidence 3",
+            'TESTAN1': 'TestAnnot2',
+            'relation': 'association'
+        })
+
+    def test(self):
+        result = pybel.from_url('http://localhost:8000/test_ns_1.belns')
+        self.assertTrue(nx.is_isomorphic(self.graph, result), 'Graphs do not match')
