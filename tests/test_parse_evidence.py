c7e6a8a050846eacd0cdc93edafb94f1212e30bf
Charles Tapley Hoyt <cthoyt@gmail.com>
2016-09-20 13:19:47 +0200

    fixed move of api

--- tests/test_parse_evidence.py
+++ tests/test_parse_evidence.py
@@ -3,11 +3,13 @@ import unittest
 from pybel import parsers
 
 
+from pybel.parsers.set_statements import sanitize_statement_lines
+
 class TestParseEvidence(unittest.TestCase):
     def test_111(self):
         statement = '''SET Evidence = "1.1.1 Easy case"'''
         expect = '''SET Evidence = "1.1.1 Easy case'''
-        lines = parsers.sanitize_statement_lines(statement.split('\n'))
+        lines = sanitize_statement_lines(statement.split('\n'))
         self.assertEqual(1, len(lines))
         line = lines[0]
         self.assertTrue(expect, line)
@@ -16,7 +18,7 @@ class TestParseEvidence(unittest.TestCase):
         statement = '''SET Evidence = "1.2.1 Forward slash break test/
 second line"'''
         expect = '''SET Evidence = "1.2.1 Forward slash break test second line"'''
-        lines = parsers.sanitize_statement_lines(statement.split('\n'))
+        lines = sanitize_statement_lines(statement.split('\n'))
         self.assertEqual(1, len(lines))
         line = lines[0]
         self.assertEqual(expect, line)
@@ -25,7 +27,7 @@ second line"'''
         statement = '''SET Evidence = "1.2.2 Forward slash break test with whitespace /
 second line"'''
         expect = '''SET Evidence = "1.2.2 Forward slash break test with whitespace second line"'''
-        lines = parsers.sanitize_statement_lines(statement.split('\n'))
+        lines = sanitize_statement_lines(statement.split('\n'))
         self.assertEqual(1, len(lines))
         line = lines[0]
         self.assertEqual(expect, line)
@@ -36,7 +38,7 @@ second line/
 third line"'''
         expect = '''SET Evidence = "1.2.3 Forward slash break test second line third line"'''
 
-        lines = parsers.sanitize_statement_lines(statement.split('\n'))
+        lines = sanitize_statement_lines(statement.split('\n'))
         self.assertEqual(1, len(lines))
         line = lines[0]
         self.assertEqual(expect, line)
@@ -45,7 +47,7 @@ third line"'''
         statement = '''SET Evidence = "3.1 Backward slash break test\\
 second line"'''
         expect = '''SET Evidence = "3.1 Backward slash break test second line"'''
-        lines = parsers.sanitize_statement_lines(statement.split('\n'))
+        lines = sanitize_statement_lines(statement.split('\n'))
         self.assertEqual(1, len(lines))
         line = lines[0]
         self.assertEqual(expect, line)
@@ -54,7 +56,7 @@ second line"'''
         statement = '''SET Evidence = "3.2 Backward slash break test with whitespace \\
 second line"'''
         expect = '''SET Evidence = "3.2 Backward slash break test with whitespace second line"'''
-        lines = parsers.sanitize_statement_lines(statement.split('\n'))
+        lines = sanitize_statement_lines(statement.split('\n'))
         self.assertEqual(1, len(lines))
         line = lines[0]
         self.assertEqual(expect, line)
@@ -64,7 +66,7 @@ second line"'''
 second line\\
 third line"'''
         expect = '''SET Evidence = "3.3 Backward slash break test second line third line"'''
-        lines = parsers.sanitize_statement_lines(statement.split('\n'))
+        lines = sanitize_statement_lines(statement.split('\n'))
         self.assertEqual(1, len(lines))
         line = lines[0]
         self.assertEqual(expect, line)
@@ -73,7 +75,7 @@ third line"'''
         statement = '''SET Evidence = "4.1 Malformed line breakcase
 second line"'''
         expect = '''SET Evidence = "4.1 Malformed line breakcase second line"'''
-        lines = parsers.sanitize_statement_lines(statement.split('\n'))
+        lines = sanitize_statement_lines(statement.split('\n'))
         self.assertEqual(1, len(lines))
         line = lines[0]
         self.assertEqual(expect, line)
@@ -83,7 +85,7 @@ second line"'''
 second line
 third line"'''
         expect = '''SET Evidence = "4.2 Malformed line breakcase second line third line"'''
-        lines = parsers.sanitize_statement_lines(statement.split('\n'))
+        lines = sanitize_statement_lines(statement.split('\n'))
         self.assertEqual(1, len(lines))
         line = lines[0]
         self.assertEqual(expect, line)
