a9f2025b588c47dc212f0984c693a14a3de181b7
Charles Tapley Hoyt <cthoyt@gmail.com>
2016-09-27 17:34:46 +0200

    added test for cli

---
+++ tests/test_cli.py
@@ -0,0 +1,14 @@
+import unittest
+
+from click.testing import CliRunner
+
+from pybel.cli import main
+
+
+class TestCli(unittest.TestCase):
+    def setUp(self):
+        self.runner = CliRunner()
+
+    def test_to_neo(self):
+        result = self.runner.invoke(main, ['to_neo', '--path', 'bel/test.bel'])
+        self.assertEqual(0, result.exit_code)
