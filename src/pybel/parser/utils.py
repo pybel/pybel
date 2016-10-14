f2ef187d0d91fff51693039ee4ccc97724398ea3
Charles Tapley Hoyt <cthoyt@gmail.com>
2016-10-14 13:57:13 +0200

    fixed output to GraphML
    list annotations still need to be crossed on compilation though, then
    this can be removed

--- src/pybel/parser/utils.py
+++ src/pybel/parser/utils.py
@@ -140,6 +140,9 @@ def flatten(d, parent_key='', sep='_'):
         new_key = parent_key + sep + k if parent_key else k
         if isinstance(v, collections.MutableMapping):
             items.extend(flatten(v, new_key, sep=sep).items())
+        elif isinstance(v, set):
+            # FIXME add cross product to graph population instead
+            items.append((new_key, ','.join(v)))
         else:
             items.append((new_key, v))
     return dict(items)
