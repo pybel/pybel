ae2e9a23b74bce4a83e22af4f8a1aa4a9bbb53f7
Charles Tapley Hoyt <cthoyt@gmail.com>
2016-10-18 16:44:03 +0200

    update docs and tests

--- tests/test_parse_utils.py
+++ tests/test_parse_utils.py
@@ -14,39 +14,6 @@ class TestUtils(unittest.TestCase):
 
         self.assertEqual(t, e)
 
-    def test_subiterator(self):
-        d = [
-            (True, 0),
-            (False, 1),
-            (False, 2),
-            (True, 1),
-            (False, 1),
-            (True, 2),
-            (False, 1),
-            (False, 2),
-            (False, 3)
-        ]
-
-        it = iter(utils.subitergroup(d, lambda x: x[0]))
-
-        matched, subit = next(it)
-        subit = iter(subit)
-        self.assertEqual(matched, (True, 0))
-        self.assertEqual((False, 1), next(subit))
-        self.assertEqual((False, 2), next(subit))
-
-        matched, subit = next(it)
-        subit = iter(subit)
-        self.assertEqual(matched, (True, 1))
-        self.assertEqual((False, 1), next(subit))
-
-        matched, subit = next(it)
-        subit = iter(subit)
-        self.assertEqual(matched, (True, 2))
-        self.assertEqual((False, 1), next(subit))
-        self.assertEqual((False, 2), next(subit))
-        self.assertEqual((False, 3), next(subit))
-
     def test_dict_matches_1(self):
         a = {
             'k1': 'v1',
@@ -202,7 +169,7 @@ class TestUtils(unittest.TestCase):
             'A': 5,
             'B': 'b',
             'C': {
-                'D': ['d','delta'],
+                'D': ['d', 'delta'],
                 'E': 'e'
             }
         }
