581f7ef9182027c7edc9b96f1f678617bb8f31ea
Charles Tapley Hoyt <cthoyt@gmail.com>
2016-11-03 22:53:50 +0100

    Added better interface for edge queries
    Also removed necessity for adding lines on bel graph load

--- src/pybel/__init__.py
+++ src/pybel/__init__.py
@@ -1,6 +1,6 @@
 """Parsing, validation, and analysis of of BEL graphs"""
 
-from __future__ import print_function
+import os
 
 from . import cli
 from . import graph
@@ -23,7 +23,31 @@ __email__ = 'charles.hoyt@scai.fraunhofer.de'
 __license__ = 'Apache 2.0 License'
 __copyright__ = 'Copyright (c) 2016 Charles Tapley Hoyt, Andrej Konotopez, Christian Ebeling'
 
+PYBEL_DATA = os.path.expanduser('~/.pybel')
+if not os.path.exists(PYBEL_DATA):
+    os.mkdir(PYBEL_DATA)
 
-def print_version():
+
+def get_version():
     """Convenience function for printing the PyBEL version"""
-    print('{} Version: {}'.format(__title__, __version__))
+    return '{} Version: {}'.format(__title__, __version__)
+
+
+def get_large_corpus(**kwargs):
+    """Gets the example large corpus"""
+    path = os.path.join(PYBEL_DATA, 'large_corpus.gpickle')
+    if os.path.exists(path):
+        return from_pickle(path)
+    g = from_url(large_corpus_url, **kwargs)
+    to_pickle(g, path)
+    return g
+
+
+def get_small_corpus(**kwargs):
+    """Gets the example small corpus"""
+    path = os.path.join(PYBEL_DATA, 'small_corpus.gpickle')
+    if os.path.exists(path):
+        return from_pickle(path)
+    g = from_url(small_corpus_url, **kwargs)
+    to_pickle(g, path)
+    return g
