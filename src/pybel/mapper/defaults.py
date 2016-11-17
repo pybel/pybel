9f8ea43059e463bf55501399b87874670ceac2eb
Charles Tapley Hoyt <cthoyt@gmail.com>
2016-11-17 16:50:01 +0100

    Delete manager
    Thanks, tim Peters!
    
    Now is better than never, but never is better than maybe sometime
    eventually

--- src/pybel/mapper/defaults.py
+++
@@ -1,8 +0,0 @@
-"""
-This file contains a dictionary of the default namespaces
-and URLs to load into a new PyBEL namespace store.
-
-See: https://wiki.openbel.org/display/BELNA/Namespaces+Overview
-"""
-
-default_namespaces = {}
