21bc4cb3ccb6c356c8b3f7b50fca4984f2dcb84f
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-01-03 12:14:53 -0500

    Better build-up and tear down for graph cache testing

--- tests/test_manager_graph.py
+++ tests/test_manager_graph.py
@@ -1,3 +1,5 @@
+import os
+import tempfile
 import unittest
 
 import pybel
@@ -7,7 +9,14 @@ from tests.constants import test_bel_1, test_bel_3, BelReconstitutionMixin
 
 class TestGraphCache(BelReconstitutionMixin, unittest.TestCase):
     def setUp(self):
-        self.gcm = GraphCacheManager('sqlite://')
+        self.dir = tempfile.mkdtemp()
+        self.db_path = os.path.join(self.dir, 'test.db')
+        self.connection = 'sqlite:///' + self.db_path
+        self.gcm = GraphCacheManager(connection=self.connection)
+
+    def tearDown(self):
+        os.remove(self.db_path)
+        os.rmdir(self.dir)
 
     def test_1(self):
         path, name, label = test_bel_1, 'PyBEL Test Document', '1.6'
@@ -22,7 +31,6 @@ class TestGraphCache(BelReconstitutionMixin, unittest.TestCase):
 
         g2 = self.gcm.load_graph(name, label)
         self.bel_1_reconstituted(g2)
-
 
     def test_get_versions(self):
         g1 = pybel.from_path(test_bel_1)
