24f1b9a1a1c1a1fad38f6e3e560f807dbd83db11
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-01-03 22:05:00 -0500

    Fixed debugging and database input/output on CLI

--- src/pybel/cli.py
+++ src/pybel/cli.py
@@ -26,16 +26,9 @@ from .manager.cache import DEFAULT_CACHE_LOCATION, CacheManager
 from .manager.graph_cache import GraphCacheManager, to_database, from_database
 
 log = logging.getLogger('pybel')
-log.setLevel(logging.DEBUG)
-
-log_levels = {
-    0: logging.INFO,
-    1: logging.DEBUG,
-}
 
 formatter = logging.Formatter('%(name)s:%(levelname)s - %(message)s')
-ch = logging.StreamHandler()
-ch.setFormatter(formatter)
+logging.basicConfig(format=formatter)
 
 fh_path = os.path.join(PYBEL_DIR, time.strftime('pybel_%Y_%m_%d_%H_%M_%S.txt'))
 fh = logging.FileHandler(fh_path)
@@ -53,7 +46,8 @@ def main():
 @main.command()
 @click.option('--path', type=click.File('r'), default=sys.stdin, help='Input BEL file file path')
 @click.option('--url', help='Input BEL file URL')
-@click.option('--database', help='Input BEL database')
+@click.option('--database-name', help='Input graph name from database')
+@click.option('--database-connection', help='Input cache location. Defaults to {}'.format(DEFAULT_CACHE_LOCATION))
 @click.option('--csv', help='Output path for *.csv')
 @click.option('--graphml', help='Output path for GraphML output. Use *.graphml for Cytoscape')
 @click.option('--json', type=click.File('w'), help='Output path for Node-link *.json')
@@ -67,18 +61,16 @@ def main():
 @click.option('--complete-origin', is_flag=True, help="Complete origin from protein to gene")
 @click.option('--log-file', type=click.File('w'), help="Optional path for verbose log output")
 @click.option('-v', '--verbose', count=True)
-def convert(path, url, database, csv, graphml, json, pickle, bel, neo, neo_context, store_default, store, lenient,
-            complete_origin,
-            log_file, verbose):
+def convert(path, url, database_name, database_connection, csv, graphml, json, pickle, bel, neo, neo_context,
+            store_default, store, lenient, complete_origin, log_file, verbose):
     """Options for multiple outputs/conversions"""
 
-    ch.setLevel(log_levels.get(verbose, 5))
-    log.addHandler(ch)
+    log.setLevel(int(5 * verbose ** 2 / 2 - 25 * verbose / 2 + 20))
 
     if url:
         g = graph.from_url(url, lenient=lenient, complete_origin=complete_origin, log_stream=log_file)
-    elif database:
-        g = from_database(database)
+    elif database_name:
+        g = from_database(database_name, connection=database_connection)
     else:
         g = graph.BELGraph(path, lenient=lenient, complete_origin=complete_origin, log_stream=log_file)
 
@@ -163,8 +155,7 @@ def ls(url, path):
     dcm = CacheManager(connection=path)
 
     if not url:
-        for url in dcm.ls():
-            click.echo(url)
+        click.echo_via_pager('\n'.join(sorted(x for x in dcm.ls() if x)))
     else:
         if url.endswith('.belns'):
             res = dcm.get_namespace(url)
