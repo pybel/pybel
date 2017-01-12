4958ccca9415c6c16ca8b992a841d1dbed6c4859
Scott Colby <scolby33@gmail.com>
2017-01-11 19:18:17 -0800

    Add note about the example extension to the docstring

--- src/pybel/ext/__init__.py
+++ src/pybel/ext/__init__.py
@@ -27,6 +27,11 @@ This works just like the standard :code:`console_scripts` entry point and the sy
 
 Your extension will then be importable as :code:`pybel.ext.name_of_your_extension`
 
+.. WARNING::
+    PyBEL does not check for collisions in extension names. Please be careful when naming your extension!
+
+See the `test extension on GitHub <https://github.com/pybel/pybel-test-extension>`_ to see a working example of an extension.
+
 :code:`pybel.ext` exists as a package on its own to trick the Python import system...if we just did this work in PyBEL's :code:`__init__.py`, there could be trouble.
 """
 from pkg_resources import iter_entry_points
