81627da2c8c39137b0a48f34f1b6b4ddf7b6bef5
Scott Colby <scolby33@gmail.com>
2017-01-11 19:06:45 -0800

    Add documentation to the extension loading code.

--- src/pybel/exthook.py
+++ src/pybel/exthook.py
@@ -1,9 +1,14 @@
-"""PyBEL Extensions"""
+"""Import hook for PyBEL Extensions"""
 from pkg_resources import iter_entry_points
 import sys
 
 
 class ExtensionImporter(object):
+    """An importer and loader for package resources registered under a particular group.
+
+    args:
+        group: a string representing the package resources entry_points group that will be used
+    """
     def __init__(self, group):
         self.group = group
 
@@ -12,9 +17,11 @@ class ExtensionImporter(object):
         return '{}.'.format(self.group)
 
     def install(self):
+        """Call this method to install the new importer to :code:`sys.meta_path`. Should probably only be done once."""
         sys.meta_path.append(self)
 
     def find_module(self, fullname, path=None):
+        """Find a module if its name starts with :code:`self.group` and is registered as a package resources entry_point."""
         if not fullname.startswith(self._group_with_dot):
             return
         end_name = fullname[len(self._group_with_dot):]
@@ -24,6 +31,7 @@ class ExtensionImporter(object):
                 return self
 
     def load_module(self, fullname):
+        """Load a module if its name starts with :code:`self.group` and is registered as a package resources entry_point."""
         if fullname in sys.modules:
             return sys.modules[fullname]
         end_name = fullname[len(self._group_with_dot):]
