86ee82004934c6f9124665712a8eb70ce3402463
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-01-29 12:53:16 +0100

    Better upgrade testing

--- tests/test_upgrade.py
+++ tests/test_upgrade.py
@@ -9,12 +9,9 @@ import unittest
 import pybel
 from pybel.canonicalize import postpend_location, decanonicalize_node
 from pybel.constants import GOCC_LATEST, FUNCTION, GOCC_KEYWORD
-from tests.constants import test_bel, test_bel_4, mock_bel_resources, mock_parse_owl_ontospy, mock_parse_owl_pybel
+from tests.constants import test_bel, test_bel_4, mock_bel_resources, mock_parse_owl_ontospy, mock_parse_owl_pybel, test_bel_thorough
 
 log = logging.getLogger('pybel')
-
-pd_path = os.path.expanduser('~/dev/bms/aetionomy/parkinsons.bel')
-small_corpus_path = os.path.expanduser('~/dev/bms/selventa/small_corpus.bel')
 
 
 class TestCanonicalizeHelper(unittest.TestCase):
@@ -43,8 +40,10 @@ class TestCanonicalize(unittest.TestCase):
             os.remove(self.path)
         os.rmdir(self.dir)
 
-    def canonicalize_helper(self, test_path):
-        original = pybel.from_path(test_path)
+    def canonicalize_helper(self, test_path, **kwargs):
+        original = pybel.from_path(test_path, **kwargs)
+
+        self.assertEqual(0, len(original.warnings))
 
         with open(self.path, 'w') as f:
             pybel.to_bel(original, f)
@@ -89,12 +88,6 @@ class TestCanonicalize(unittest.TestCase):
     def test_canonicalize_4(self, m1, m2, m3):
         self.canonicalize_helper(test_bel_4)
 
-    @unittest.skipUnless(os.path.exists(small_corpus_path), 'Small Corpus is missing')
-    def test_small_corpus(self):
-        self.maxDiff = None
-        self.canonicalize_helper(small_corpus_path)
-
-    @unittest.skipUnless(os.path.exists(pd_path), 'PD Assembly is missing')
-    def test_parkinsons(self):
-        self.maxDiff = None
-        self.canonicalize_helper(pd_path)
+    @mock_bel_resources
+    def test_thorough(self, mock_get):
+        self.canonicalize_helper(test_bel_thorough, allow_nested=True)
