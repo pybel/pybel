ff861bd0eaba5dec0ced6415ac255b79896a5319
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-01-30 11:43:44 +0100

    Remove unnecessary doctoring in base exception

--- tests/test_extras.py
+++ tests/test_extras.py
@@ -3,7 +3,6 @@
 import os
 import unittest
 
-from pybel.exceptions import PyBelWarning
 from pybel.parser.baseparser import nest, BaseParser
 from pybel.parser.language import amino_acid
 from pybel.parser.parse_exceptions import PlaceholderAminoAcidWarning
@@ -36,13 +35,6 @@ class TestRandom(unittest.TestCase):
     def test_bad_aminoAcid(self):
         with self.assertRaises(PlaceholderAminoAcidWarning):
             amino_acid.parseString('X')
-
-    def test_pybelexception_str(self):
-        e = PyBelWarning('XXX')
-        self.assertEqual("PyBelWarning - XXX", str(e))
-
-        with self.assertRaises(PyBelWarning):
-            raise e
 
     def test_unimplemented_mapping(self):
         with self.assertRaises(NotImplementedError):
