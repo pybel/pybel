2f212663b7e4dc5c1ac747f0c93f362296de989b
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-02-12 19:20:34 +0100

    Bump version: 0.3.8-dev → 0.3.8

--- src/pybel/__init__.py
+++ src/pybel/__init__.py
@@ -58,7 +58,7 @@ from .manager.graph_cache import to_database, from_database
 
 __all__ = ['BELGraph', 'to_database', 'from_database', 'to_bel'] + list(io.__all__)
 
-__version__ = '0.3.8-dev'
+__version__ = '0.3.8'
 
 __title__ = 'PyBEL'
 __description__ = 'Parsing, validation, and analysis of BEL graphs'
