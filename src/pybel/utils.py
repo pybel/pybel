c87d761580f70a6871f80ab9cc529936425fa4d1
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-03-01 11:30:40 +0100

    Update utils docs

--- src/pybel/utils.py
+++ src/pybel/utils.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import collections
 import logging
-from collections import defaultdict
+from collections import defaultdict, MutableMapping
 from configparser import ConfigParser
 
 import networkx as nx
@@ -51,8 +50,11 @@ def download_url(url):
 
 def expand_dict(flat_dict, sep='_'):
     """Expands a flattened dictionary
+
     :param flat_dict: a nested dictionary that has been flattened so the keys are composite
-    :param sep: the seperator between concatenated keys
+    :type flat_dict: dict
+    :param sep: the separator between concatenated keys
+    :type sep: str
     """
     res = {}
     rdict = defaultdict(list)
@@ -71,14 +73,21 @@ def expand_dict(flat_dict, sep='_'):
 
 
 def flatten_dict(d, parent_key='', sep='_'):
-    """Flattens a nested dictionary
+    """Flattens a nested dictionary.
 
-    Borrowed from http://stackoverflow.com/a/6027615
+    :param d: A nested dictionary
+    :type d: dict or MutableMapping
+    :param parent_key: The parent's key. This is a value for tail recursion, so don't set it yourself.
+    :type parent_key: str
+    :param sep: The separator used between dictionary levels
+    :type sep: str
+
+    .. seealso:: http://stackoverflow.com/a/6027615
     """
     items = []
     for k, v in d.items():
         new_key = parent_key + sep + k if parent_key else k
-        if isinstance(v, collections.MutableMapping):
+        if isinstance(v, (dict, MutableMapping)):
             items.extend(flatten_dict(v, new_key, sep=sep).items())
         elif isinstance(v, (set, list)):
             items.append((new_key, ','.join(v)))
@@ -108,7 +117,7 @@ def flatten_graph_data(graph):
 
 def list2tuple(l):
     """turns a nested list to a nested tuple"""
-    if not isinstance(l, list):
-        return l
-    else:
+    if isinstance(l, list):
         return tuple(list2tuple(e) for e in l)
+    else:
+        return l
