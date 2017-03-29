3933b8eeabc3324359dd30db9b45751aa9796a26
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-03-29 19:42:42 +0200

    Update docs

--- src/pybel/parser/baseparser.py
+++ src/pybel/parser/baseparser.py
@@ -49,6 +49,7 @@ def one_of_tags(tags, canonical_tag, identifier):
 
 
 def triple(subject, relation, obj):
+    """Builds a simple triple in PyParsing that has a ``subject relation object`` format"""
     return And([Group(subject)(SUBJECT), relation(RELATION), Group(obj)(OBJECT)])
 
 
@@ -64,18 +65,24 @@ class BaseParser:
         if streamline:
             self.streamline()
 
-    def parse_lines(self, l):
-        """Parses multiple lines successively"""
-        return [self.parseString(line) for line in l]
-
-    def parseString(self, s):
-        """Parses a string with the language represented by this parser
-        :param s: input string
-        :type s: str
+    def parse_lines(self, lines):
+        """Parses multiple lines in succession
+        
+        :return: An list of the resulting parsed lines' tokens
+        :rtype: list
         """
-        return self.language.parseString(s)
+        return [self.parseString(line) for line in lines]
+
+    def parseString(self, line):
+        """Parses a string with the language represented by this parser
+        
+        :param line: A string representing an instance of this parser's language
+        :type line: str
+        """
+        return self.language.parseString(line)
 
     def streamline(self):
+        """Streamlines the language represented by this parser to make queries run faster"""
         t = time.time()
         self.language.streamline()
         log.info('Finished streamlining %s in %.02fs', self.__class__.__name__, time.time() - t)
