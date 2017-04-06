9563e9ed77dd4f561a8a73349ba4740a54a61955
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-04-07 00:10:28 +0200

    Update filesystem and connection management in CLI test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -15,12 +15,14 @@ from pybel.io import from_pickle, from_json, from_path
 from pybel.manager.database_io import from_database
 from tests.constants import test_bel_simple, BelReconstitutionMixin, mock_bel_resources, test_bel_thorough, \
     expected_test_thorough_metadata
+from tests.constants import TemporaryCacheMixin
 
 log = logging.getLogger(__name__)
 
 
-class TestCli(BelReconstitutionMixin, unittest.TestCase):
+class TestCli(TemporaryCacheMixin, BelReconstitutionMixin):
     def setUp(self):
+        super(TestCli, self).setUp()
         self.runner = CliRunner()
 
     @mock_bel_resources
@@ -30,18 +32,17 @@ class TestCli(BelReconstitutionMixin, unittest.TestCase):
             test_csv = os.path.abspath('test.csv')
             test_gpickle = os.path.abspath('test.gpickle')
             test_canon = os.path.abspath('test.bel')
-            connection = 'sqlite:///' + os.path.abspath('test.db')
 
             args = [
                 'convert',
                 # Input
                 '--path', test_bel_thorough,
-                '--connection', connection,
+                '--connection', self.connection,
                 # Outputs
                 '--csv', test_csv,
                 '--pickle', test_gpickle,
                 '--bel', test_canon,
-                '--store-connection', connection,
+                '--store-connection', self.connection,
                 '--allow-nested'
             ]
 
@@ -53,19 +54,18 @@ class TestCli(BelReconstitutionMixin, unittest.TestCase):
             self.bel_thorough_reconstituted(from_pickle(test_gpickle))
             self.bel_thorough_reconstituted(from_path(test_canon))
             self.bel_thorough_reconstituted(from_database(expected_test_thorough_metadata[METADATA_NAME],
-                                                          connection=connection))
+                                                          connection=self.connection))
 
     @mock_bel_resources
     def test_convert_json(self, mock_get):
         with self.runner.isolated_filesystem():
             test_json = os.path.abspath('test.json')
-            connection = 'sqlite:///' + os.path.abspath('test.db')
 
             args = [
                 'convert',
                 '--path', test_bel_thorough,
                 '--json', test_json,
-                '--connection', connection,
+                '--connection', self.connection,
                 '--allow-nested'
             ]
 
@@ -89,11 +89,10 @@ class TestCli(BelReconstitutionMixin, unittest.TestCase):
             self.skipTest("Can't connect to Neo4J server")
         else:
             with self.runner.isolated_filesystem():
-                connection = 'sqlite:///' + os.path.abspath('test.db')
                 args = [
                     'convert',
                     '--path', test_bel_simple,
-                    '--connection', connection,
+                    '--connection', self.connection,
                     '--neo', neo_path,
                     '--neo-context', test_context
                 ]
