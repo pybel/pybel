0af1aef7df8d51e052c1041114d66ab648f1c30e
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-04-09 11:15:56 +0200

    Refactored version exception handling

---
+++ src/pybel/io/io_exceptions.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from ..exceptions import PyBelWarning
+
+
+class ImportVersionWarning(PyBelWarning, ValueError):
+    """Raised when trying to import data from an old version of PyBEL"""
+    def __init__(self, actual_version_tuple, minimum_version_tuple):
+        PyBelWarning.__init__(self, actual_version_tuple, minimum_version_tuple)
+        self.actual_tuple = actual_version_tuple
+        self.minimum_tuple = minimum_version_tuple
+
+    def __str__(self):
+        actual_s = '.'.join(map(str, self.actual_tuple))
+        minimum_s = '.'.join(map(str, self.minimum_tuple))
+        return 'Tried importing from PyBEL v{}. Need at least v{}'.format(actual_s, minimum_s)
--- src/pybel/io/utils.py
+++ src/pybel/io/utils.py
@@ -2,20 +2,29 @@
 
 """This module contains helper functions for other IO functions."""
 
+from .io_exceptions import ImportVersionWarning
 from ..utils import tokenize_version
+
+#: The last PyBEL version where the graph data definition changed
+PYBEL_MINIMUM_IMPORT_VERSION = (0, 4, 1)
+
+
+def raise_for_old_graph(graph):
+    graph_version = tokenize_version(graph.pybel_version)
+    if graph_version < PYBEL_MINIMUM_IMPORT_VERSION:
+        raise ImportVersionWarning(graph_version, PYBEL_MINIMUM_IMPORT_VERSION)
 
 
 def ensure_version(graph, check_version=True):
-    """Ensure that the graph was produced by a minimum of PyBEL v0.4.1, which was the last release with a change
-    in the data structure
+    """Ensure that the graph was produced by a minimum of PyBEL v:data:`PYBEL_MINIMUM_IMPORT_VERSION`, which was the 
+    last release with a change in the graph data definition.
 
     :param graph: A BEL Graph
     :type graph: BELGraph
     :param check_version: Should the version be checked, or should the graph just be returned without inspection
     :type check_version: bool
     """
-    minimum_version = (0, 4, 1)
-    graph_version = tokenize_version(graph.pybel_version)
-    if check_version and graph_version < minimum_version:
-        raise ValueError('Tried importing from version {}. Need at least {}'.format(graph_version, minimum_version))
+    if check_version:
+        raise_for_old_graph(graph)
+
     return graph
