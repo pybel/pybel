d49ddb7f0ffb03535e652a5f2668694656c4f8e2
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-05-08 15:15:59 +0200

    Remove CLI testing
    Will have to resume later

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -19,6 +19,7 @@ from tests.constants import test_bel_simple, mock_bel_resources, test_bel_thorou
 log = logging.getLogger(__name__)
 
 
+@unittest.skipUnless('PYBEL_TEST_THOROUGH' in os.environ)
 class TestCli(TemporaryCacheMixin, BelReconstitutionMixin):
     def setUp(self):
         super(TestCli, self).setUp()
