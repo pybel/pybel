86afe81e4302921d7e35374b5664074c94494cce
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-06-07 12:15:33 +0200

    Bump version: 0.5.10 → 0.5.11-dev

--- src/pybel/__init__.py
+++ src/pybel/__init__.py
@@ -75,7 +75,7 @@ __all__ = (
     database_io.__all__
 )
 
-__version__ = '0.5.10'
+__version__ = '0.5.11-dev'
 
 __title__ = 'PyBEL'
 __description__ = 'Parsing, validation, and data exchange of BEL graphs'
