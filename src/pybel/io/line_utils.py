e4fe7772f29cfbd1e9916f48f5259b4ed93d4c8c
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-09-05 16:38:29 -0400

    Update documentation

--- src/pybel/io/line_utils.py
+++ src/pybel/io/line_utils.py
@@ -3,11 +3,11 @@
 """This module contains helper functions for reading BEL scripts"""
 
 import logging
-import re
 import time
-from collections import defaultdict, Counter
 
+import re
 import requests.exceptions
+from collections import defaultdict, Counter
 from pyparsing import ParseException
 from sqlalchemy.exc import OperationalError
 
@@ -52,10 +52,9 @@ def parse_lines(graph, lines, manager=None, allow_nested=False, citation_clearin
 
     :param BELGraph graph: A BEL graph
     :param iter[str] lines: An iterable over lines of BEL script
-    :param manager: An RFC-1738 database connection string, a pre-built :class:`CacheManager`, a pre-built 
-                    :class:`MetadataParser`, or ``None`` for default connection
+    :param manager: An RFC-1738 database connection string, a pre-built :class:`CacheManager`, or ``None`` for
+                    default connection
     :type manager: None or str or CacheManager
-
     :param bool allow_nested: If true, turns off nested statement failures
     :param bool citation_clearing: Should :code:`SET Citation` statements clear evidence and all annotations?
                                    Delegated to :class:`pybel.parser.ControlParser`
@@ -68,9 +67,9 @@ def parse_lines(graph, lines, manager=None, allow_nested=False, citation_clearin
     """
     docs, definitions, statements = split_file_to_annotations_and_definitions(lines)
 
-    manager= build_manager(manager)
+    manager = build_manager(manager)
 
-    metadata_parser =  MetadataParser(manager, allow_redefinition=kwargs.get('allow_redefinition'))
+    metadata_parser = MetadataParser(manager, allow_redefinition=kwargs.get('allow_redefinition'))
 
     parse_document(graph, docs, metadata_parser)
 
