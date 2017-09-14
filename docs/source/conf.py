f7704644261064a12a42fe3b205859388f127174
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-09-14 12:03:19 +0200

    Add intersphinx reference to sqlalchemy

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -361,6 +361,7 @@ intersphinx_mapping = {
     'python': ('https://docs.python.org/3', None),
     'networkx': ('https://networkx.github.io/', None),
     'py2neo': ('http://py2neo.org/v3/', None),
+    'sqlalchemy': ('http://www.sqlalchemy.org/docs/', None)
 }
 
 autodoc_member_order = 'bysource'
