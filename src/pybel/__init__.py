2bbeca856003637437f0cdda9d8283070f9de129
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-09-26 16:16:00 +0200

    Bump version: 0.9.1-dev → 0.9.1

--- src/pybel/__init__.py
+++ src/pybel/__init__.py
@@ -78,7 +78,7 @@ __all__ = (
     cache_manager.__all__
 )
 
-__version__ = '0.9.1-dev'
+__version__ = '0.9.1'
 
 __title__ = 'PyBEL'
 __description__ = 'Parsing, validation, and data exchange of BEL graphs'
