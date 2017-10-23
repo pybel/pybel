56f856250f765c646259d0a91abca3f589ce9e8f
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-10-23 12:12:37 +0200

    Add short flag for storing parts

--- src/pybel/cli.py
+++ src/pybel/cli.py
@@ -63,7 +63,7 @@ def main():
 @click.option('--bel', type=click.File('w'), help='Output canonical BEL')
 @click.option('--neo', help="Connection string for neo4j upload")
 @click.option('--neo-context', help="Optional context for neo4j upload")
-@click.option('--store-default', is_flag=True, help="Stores to default cache at {}".format(get_cache_connection()))
+@click.option('-s', '--store-default', is_flag=True, help="Stores to default cache at {}".format(get_cache_connection()))
 @click.option('--store-connection', help="Database connection string")
 @click.option('--allow-naked-names', is_flag=True, help="Enable lenient parsing for naked names")
 @click.option('--allow-nested', is_flag=True, help="Enable lenient parsing for nested statements")
