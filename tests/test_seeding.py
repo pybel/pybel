4eee0fbd0d51c430b7ff7763fe8fd4bf321c44f9
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-10-24 17:53:57 +0200

    Update seeding tests

--- tests/test_seeding.py
+++ tests/test_seeding.py
@@ -2,8 +2,11 @@
 
 from pybel.examples import sialic_acid_graph
 from pybel.examples.sialic_acid_example import shp2
+from pybel.manager.models import Edge, Namespace, Network
 from tests.constants import TemporaryCacheClsMixin
 from tests.mocks import mock_bel_resources
+
+chebi_url = 'https://arty.scai.fraunhofer.de/artifactory/bel/namespace/chebi/chebi-20170725.belns'
 
 
 class TestSeeding(TemporaryCacheClsMixin):
@@ -20,6 +23,36 @@ class TestSeeding(TemporaryCacheClsMixin):
             cls.manager.insert_graph(sialic_acid_graph, store_parts=True)
 
         insert()
+
+    def test_namespace_existence(self):
+        a = 'https://arty.scai.fraunhofer.de/artifactory/bel/namespace/hgnc-human-genes/hgnc-human-genes-20170725.belns'
+        n = self.manager.session.query(Namespace).filter(Namespace.url == a).one()
+
+    def test_namespace_existence_b(self):
+        ns = self.manager.session.query(Namespace).filter(Namespace.url == chebi_url).one()
+        self.assertIsNotNone(ns)
+
+    def test_sialic_acid_in_node_store(self):
+        r = 'sialic acid'
+
+        n = self.manager.get_namespace_entry(chebi_url, r)
+        self.assertIsNotNone(n)
+
+        self.assertEqual(r, n.name)
+
+    def test_namespace_existence_c(self):
+        a = 'https://arty.scai.fraunhofer.de/artifactory/bel/namespace/go-biological-process/go-biological-process-20170725.belns'
+        self.manager.session.query(Namespace).filter(Namespace.url == a).one()
+
+    def test_network_existence(self):
+        networks = self.manager.session.query(Network).all()
+
+        self.assertEqual(1, len(networks))
+
+    def test_edge_existence(self):
+        edges = self.manager.session.query(Edge).all()
+
+        self.assertEqual(11, len(edges))
 
     def test_seed_by_pmid(self):
         pmids = ['26438529']
