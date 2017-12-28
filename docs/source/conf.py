8b3023b4f03ff281ce90ecc35816b41234a44d0b
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-12-28 13:46:18 +0100

    Revert "Bump version: 0.9.7 → 0.9.8-dev"
    This reverts commit 5ba185e5b7f359c327b032d5c2f2ebfde5726c68.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -68,7 +68,7 @@ author = u'Charles Tapley Hoyt'
 #
 
 # The full version, including alpha/beta/rc tags.
-release = '0.9.8-dev'
+release = '0.9.7'
 
 # The short X.Y version.
 parsed_version = re.match(
