979e518228a66374a3bbeaa339b79eed7ba98996
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-02-07 17:48:51 +0100

    Update version

--- src/pybel/constants.py
+++ src/pybel/constants.py
@@ -21,7 +21,7 @@ log = getLogger(__name__)
 VERSION = '0.10.2-dev'
 
 #: The last PyBEL version where the graph data definition changed
-PYBEL_MINIMUM_IMPORT_VERSION = 0, 10, 0
+PYBEL_MINIMUM_IMPORT_VERSION = 0, 11, 0
 
 BELFRAMEWORK_DOMAIN = 'http://resource.belframework.org'
 OPENBEL_DOMAIN = 'http://resources.openbel.org'
