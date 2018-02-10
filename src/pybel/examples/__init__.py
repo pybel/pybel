43e41e14b82c68d41716854d25d93122ab6900bb
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-02-10 17:21:48 +0100

    Add BRAF example graph
    References #228

--- src/pybel/examples/__init__.py
+++ src/pybel/examples/__init__.py
@@ -2,6 +2,7 @@
 
 """This directory contains example networks, precompiled as BEL graphs that are appropriate to use in examples."""
 
+from .braf_example import braf_graph
 from .egf_example import egf_graph
 from .sialic_acid_example import sialic_acid_graph
 from .statin_example import statin_graph
@@ -10,4 +11,5 @@ __all__ = [
     'egf_graph',
     'sialic_acid_graph',
     'statin_graph',
+    'braf_graph',
 ]
