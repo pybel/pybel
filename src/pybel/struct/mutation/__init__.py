daa5b84898712a5b2933918c63ab3f7f45b6baf4
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-02-10 13:27:24 +0100

    Add knowledge transfer function

--- src/pybel/struct/mutation/__init__.py
+++ src/pybel/struct/mutation/__init__.py
@@ -2,9 +2,11 @@
 
 """This module contains functions that mutate or make transformations on a network"""
 
-from . import metadata
+from . import metadata, transfer
 from .metadata import *
+from .transfer import *
 
 __all__ = (
-    metadata.__all__
+    metadata.__all__ +
+    transfer.__all__
 )
---
+++ src/pybel/struct/mutation/transfer.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+"""This module facilitates the transfer of knowledge through ontological relationships"""
+
+from ...constants import ANNOTATIONS, CAUSAL_RELATIONS, CITATION, EVIDENCE, IS_A, OBJECT, RELATION, SUBJECT
+from ...dsl.nodes import BaseEntity
+
+__all__ = [
+    'infer_child_relations'
+]
+
+
+def iter_children(graph, node):
+    """Iterates over children of the node
+
+    :param pybel.BELGraph graph:
+    :param tuple node:
+    :rtype: iter[tuple]
+    """
+    for u, _, d in graph.in_edges(node, data=True):
+        if d[RELATION] != IS_A:
+            continue
+        yield u
+
+
+def transfer_causal_edges(graph, source, target):
+    """Transfer causal edges that the source has to the target
+
+    :param pybel.BELGraph graph:
+    :param tuple source:
+    :param tuple target:
+    """
+    for _, v, k, d in graph.out_edges(source, keys=True, data=True):
+        if d[RELATION] not in CAUSAL_RELATIONS:
+            continue
+
+        graph.add_qualified_edge(
+            target,
+            v,
+            relation=d[RELATION],
+            evidence=d[EVIDENCE],
+            citation=d[CITATION],
+            annotations=d.get(ANNOTATIONS),
+            subject_modifier=d.get(SUBJECT),
+            object_modifier=d.get(OBJECT)
+        )
+
+    for u, _, k, d in graph.in_edges(source, keys=True, data=True):
+        if d[RELATION] not in CAUSAL_RELATIONS:
+            continue
+
+        graph.add_qualified_edge(
+            u,
+            target,
+            relation=d[RELATION],
+            evidence=d[EVIDENCE],
+            citation=d[CITATION],
+            annotations=d.get(ANNOTATIONS),
+            subject_modifier=d.get(SUBJECT),
+            object_modifier=d.get(OBJECT)
+        )
+
+
+def infer_child_relations(graph, node):
+    """Propagates causal relations to children
+
+    :param pybel.BELGraph graph: A BEL graph
+    :param node: A PyBEL node tuple, on which to propagate the children's relations
+    :type node: tuple or BaseEntity
+    """
+    if isinstance(node, BaseEntity):
+        node = node.as_tuple()
+
+    for child in iter_children(graph, node):
+        transfer_causal_edges(graph, node, child)
+        infer_child_relations(graph, child)
