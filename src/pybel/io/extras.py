2ad0910b57ebdef851e1a80269feee619bf15ed1
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-04-13 15:05:16 +0200

    Add argument for keeping edge data

--- src/pybel/io/extras.py
+++ src/pybel/io/extras.py
@@ -23,12 +23,13 @@ __all__ = [
 log = logging.getLogger(__name__)
 
 
-def to_graphml(graph, file):
+def to_graphml(graph, file, keep_edge_data=True):
     """Writes this graph to GraphML XML file using :func:`networkx.write_graphml`. The .graphml file extension is
     suggested so Cytoscape can recognize it.
 
     :param BELGraph graph: A BEL graph
     :param file file: A file or file-like object
+    :param bool keep_edge_data: Should the edge data be kept?
     """
     g = nx.MultiDiGraph()
 
@@ -36,7 +37,7 @@ def to_graphml(graph, file):
         g.add_node(node, json=json.dumps(data), function=data[FUNCTION])
 
     for u, v, key, data in graph.edges(data=True, keys=True):
-        g.add_edge(u, v, key=key, attr_dict=flatten_dict(data))
+        g.add_edge(u, v, key=key, attr_dict=(flatten_dict(data) if keep_edge_data else None))
 
     nx.write_graphml(g, file)
 
