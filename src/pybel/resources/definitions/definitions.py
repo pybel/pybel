6e0c73e17fc866dfd08f160f2f202d85df623824
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-05-22 23:38:23 -0400

    Remove unused code

--- src/pybel/resources/definitions/definitions.py
+++ src/pybel/resources/definitions/definitions.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
 
-import hashlib
-import json
 import logging
 import os
-from configparser import ConfigParser
-
 import requests.exceptions
 import six
+from configparser import ConfigParser
 
 from ..exc import EmptyResourceError, InvalidResourceError, MissingResourceError
 from ..utils import download, is_url
@@ -16,8 +13,6 @@ __all__ = [
     'parse_bel_resource',
     'get_lines',
     'get_bel_resource',
-    'hash_names',
-    'get_bel_resource_hash',
 ]
 
 log = logging.getLogger(__name__)
@@ -108,43 +103,3 @@ def get_bel_resource(location):
         raise EmptyResourceError(location)
 
     return result
-
-
-def _names_to_bytes(names):
-    """Reproducibly converts an iterable of strings to bytes
-
-    :param iter[str] names: An iterable of strings
-    :rtype: bytes
-    """
-    names = sorted(names)
-    names_bytes = json.dumps(names).encode('utf8')
-    return names_bytes
-
-
-def hash_names(names, hash_function=None):
-    """Returns the hash of an iterable of strings, or a dict if multiple hash functions given.
-
-    :param iter[str] names: An iterable of strings
-    :param hash_function: A hash function or list of hash functions, like :func:`hashlib.md5` or :func:`hashlib.sha512`
-    :rtype: str
-    """
-    hash_function = hash_function or hashlib.md5
-    names_bytes = _names_to_bytes(names)
-    return hash_function(names_bytes).hexdigest()
-
-
-def get_bel_resource_hash(location, hash_function=None):
-    """Gets a BEL resource file and returns its semantic hash
-
-    :param str location: URL of a resource
-    :param hash_function: A hash function or list of hash functions, like :func:`hashlib.md5` or :code:`hashlib.sha512`
-    :return: The hexadecimal digest of the hash of the values in the resource
-    :rtype: str
-    :raises: pybel.resources.exc.ResourceError
-    """
-    resource = get_bel_resource(location)
-
-    return hash_names(
-        resource['Values'],
-        hash_function=hash_function
-    )
