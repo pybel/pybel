e59e98fae97d1fbeb4392524831418dc3fe2c49c
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-05-26 21:19:35 +0200

    Revert collation but specify in author table

--- src/pybel/manager/models.py
+++ src/pybel/manager/models.py
@@ -664,7 +664,7 @@ class Author(Base):
     __tablename__ = AUTHOR_TABLE_NAME
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(255), nullable=False, unique=True, index=True)
+    name = Column(String(255), nullable=False, unique=True, index=True, collation='utf8_unicode_ci')
 
     @staticmethod
     def name_contains(name_query):
