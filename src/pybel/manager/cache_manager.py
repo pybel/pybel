0ed9fe11ee5bb8a2a407fede7ff0ac72a52dcb0d
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-05-31 15:31:53 +0200

    Second reorganisation of manager

--- src/pybel/manager/cache_manager.py
+++ src/pybel/manager/cache_manager.py
@@ -17,7 +17,8 @@ from itertools import chain, groupby
 
 import six
 from six import string_types
-from sqlalchemy import and_, exists, func
+from sqlalchemy import and_, create_engine, exists, func
+from sqlalchemy.orm import scoped_session, sessionmaker
 from tqdm import tqdm
 
 from .base_manager import BaseManager
@@ -1504,20 +1505,89 @@ class InsertManager(NamespaceManager, AnnotationManager, LookupManager):
         ]
 
 
-class Manager(QueryManager, InsertManager, NetworkManager):
-    """The definition cache manager takes care of storing BEL namespace and annotation files for later use. It uses
-    SQLite by default for speed and lightness, but any database can be used with its SQLAlchemy interface.
+class _Manager(QueryManager, InsertManager, NetworkManager):
+    """A wrapper around PyBEL managers that can be directly instantiated with an engine and session."""
+
+
+class Manager(_Manager):
+    """Creates a connection to database and a persistent session using SQLAlchemy.
+
+    A custom default can be set as an environment variable with the name :data:`pybel.constants.PYBEL_CONNECTION`,
+    using an `RFC-1738 <http://rfc.net/rfc1738.html>`_ string. For example, a MySQL string can be given with the
+    following form:
+
+    :code:`mysql+pymysql://<username>:<password>@<host>/<dbname>?charset=utf8[&<options>]`
+
+    A SQLite connection string can be given in the form:
+
+    ``sqlite:///~/Desktop/cache.db``
+
+    Further options and examples can be found on the SQLAlchemy documentation on
+    `engine configuration <http://docs.sqlalchemy.org/en/latest/core/engines.html>`_.
     """
+
+    def __init__(self, connection=None, echo=False, autoflush=None, autocommit=None, expire_on_commit=None,
+                 scopefunc=None):
+        """
+        :param Optional[str] connection: An RFC-1738 database connection string. If ``None``, tries to load from the
+         environment variable ``PYBEL_CONNECTION`` then from the config file ``~/.config/pybel/config.json`` whose
+         value for ``PYBEL_CONNECTION`` defaults to :data:`pybel.constants.DEFAULT_CACHE_LOCATION`.
+        :param bool echo: Turn on echoing sql
+        :param Optional[bool] autoflush: Defaults to True if not specified in kwargs or configuration.
+        :param Optional[bool] autocommit: Defaults to False if not specified in kwargs or configuration.
+        :param Optional[bool] expire_on_commit: Defaults to False if not specified in kwargs or configuration.
+        :param scopefunc: Scoped function to pass to :func:`sqlalchemy.orm.scoped_session`
+
+        From the Flask-SQLAlchemy documentation:
+
+        An extra key ``'scopefunc'`` can be set on the ``options`` dict to
+        specify a custom scope function.  If it's not provided, Flask's app
+        context stack identity is used. This will ensure that sessions are
+        created and removed with the request/response cycle, and should be fine
+        in most cases.
+        """
+        self.connection = get_cache_connection(connection)
+
+        engine = create_engine(self.connection, echo=echo)
+
+        self.autoflush = autoflush if autoflush is not None else config.get('PYBEL_MANAGER_AUTOFLUSH', False)
+        self.autocommit = autocommit if autocommit is not None else config.get('PYBEL_MANAGER_AUTOCOMMIT', False)
+        self.expire_on_commit = expire_on_commit if expire_on_commit is not None else config.get(
+            'PYBEL_MANAGER_AUTOEXPIRE', True)
+        self.scopefunc = scopefunc
+
+        log.info(
+            'auto flush: %s, auto commit: %s, expire on commmit: %s',
+            self.autoflush,
+            self.autoflush,
+            self.expire_on_commit
+        )
+
+        #: A SQLAlchemy session maker
+        self.session_maker = sessionmaker(
+            bind=engine,
+            autoflush=self.autoflush,
+            autocommit=self.autocommit,
+            expire_on_commit=self.expire_on_commit,
+        )
+
+        #: A SQLAlchemy session object
+        session = scoped_session(self.session_maker, scopefunc=self.scopefunc)
+
+        super(Manager, self).__init__(engine=engine, session=session)
+
+        self.create_all()
 
     @staticmethod
     def ensure(connection=None, *args, **kwargs):
         """A convenience method for turning a string into a connection, or passing a :class:`Manager` through.
 
+        Args and kwargs are passed through to the constructor for :class:`Manager` if the given connection is not a
+        string.
+
         :param connection: An RFC-1738 database connection string, a pre-built :class:`Manager`, or ``None``
                             for default connection
         :type connection: Optional[str or Manager]
-        :param list args: Positional arguments to pass to the constructor of :class:`Manager`
-        :param dict kwargs: Keyword arguments to pass to the constructor of :class:`Manager`
         :rtype: Manager
         """
         if connection is None or isinstance(connection, string_types):
