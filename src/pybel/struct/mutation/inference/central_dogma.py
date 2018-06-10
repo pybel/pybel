8ea473a9b2a9b0eefc1663a39a307666fe35ef9f
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-06-10 20:12:51 +0200

    Update central dogma inference functions
    - removed unused function
    - fixed creation of corresponding central dogma nodes since we can’t
    assume the data is a DSL object
    - update testing for more coverage

--- src/pybel/struct/mutation/inference/central_dogma.py
+++ src/pybel/struct/mutation/inference/central_dogma.py
@@ -1,57 +1,39 @@
 # -*- coding: utf-8 -*-
 
 from ...pipeline import in_place_transformation
-from ....constants import FUNCTION, MIRNA, NAMESPACE, PROTEIN, RNA, VARIANTS
+from ....constants import FUNCTION, IDENTIFIER, MIRNA, NAME, NAMESPACE, PROTEIN, RNA, VARIANTS
+from ....dsl import gene, rna
 
 __all__ = [
-    'infer_central_dogmatic_translations_by_namespace',
     'infer_central_dogmatic_transcriptions',
     'infer_central_dogmatic_translations',
     'infer_central_dogma',
 ]
 
 
-def _infer_converter_helper(node, data, new_function):
-    new_tup = list(node)
-    new_tup[0] = new_function
-    new_tup = tuple(new_tup)
-    new_dict = data.copy()
-    new_dict[FUNCTION] = new_function
-    return new_tup, new_dict
-
-
 @in_place_transformation
-def infer_central_dogmatic_translations_by_namespace(graph, namespaces):
-    """Adds the missing origin :class:`pybel.dsl.rna` and RNA-Protein translation edge for all proteins.
+def infer_central_dogmatic_translations(graph):
+    """Add the missing origin RNA and RNA-Protein translation edge for all Protein entities.
 
     :param pybel.BELGraph graph: A BEL graph
-    :param str or iter[str] namespaces: The namespaces over which to do this
     """
-    namespaces = {namespaces} if isinstance(namespaces, str) else set(namespaces)
-
-    for _, data in graph.nodes(data=True):
+    for protein_node, data in graph.nodes(data=True):
         if data[FUNCTION] != PROTEIN:
             continue
 
-        if NAMESPACE not in data:
+        namespace = data.get(NAMESPACE)
+        if namespace is None:
             continue
 
         if VARIANTS in data:
             continue
 
-        if data[NAMESPACE] not in namespaces:
-            continue
-
-        graph.add_translation(data.get_rna(), data)
-
-
-@in_place_transformation
-def infer_central_dogmatic_translations(graph):
-    """Add the missing origin RNA and RNA-Protein translation edge for all HGNC Protein entities.
-
-    :param pybel.BELGraph graph: A BEL graph
-    """
-    infer_central_dogmatic_translations_by_namespace(graph, 'HGNC')
+        rna_node = rna(
+            namespace=namespace,
+            name=data.get(NAME),
+            identifier=data.get(IDENTIFIER),
+        )
+        graph.add_translation(rna_node, protein_node)
 
 
 @in_place_transformation
@@ -60,17 +42,23 @@ def infer_central_dogmatic_transcriptions(graph):
 
     :param pybel.BELGraph graph: A BEL graph
     """
-    for _, data in graph.nodes(data=True):
+    for rna_node, data in graph.nodes(data=True):
         if data[FUNCTION] not in {MIRNA, RNA}:
             continue
 
-        if NAMESPACE not in data:
+        namespace = data.get(NAMESPACE)
+        if namespace is None:
             continue
 
         if VARIANTS in data:
             continue
 
-        graph.add_transcription(data.get_gene(), data)
+        gene_node = gene(
+            namespace=namespace,
+            name=data.get(NAME),
+            identifier=data.get(IDENTIFIER),
+        )
+        graph.add_transcription(gene_node, rna_node)
 
 
 @in_place_transformation
