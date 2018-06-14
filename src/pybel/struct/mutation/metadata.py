e53418f9ce4e462144406283931f1579ce8141db
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-06-14 10:04:14 +0200

    Add functions for add/remove annotation
    References #304
    
    Still needs testing.
    
    Also moved some other testing stuff around

--- src/pybel/struct/mutation/metadata.py
+++ src/pybel/struct/mutation/metadata.py
@@ -1,17 +1,70 @@
 # -*- coding: utf-8 -*-
 
+"""Functions to modify the metadata of graphs, their edges, and their nodes."""
+
+import logging
+
+from ..pipeline import in_place_transformation
 from ...constants import ANNOTATIONS
 
 __all__ = [
     'strip_annotations',
+    'add_annotation_value',
+    'remove_annotation_value',
 ]
 
+log = logging.getLogger(__name__)
 
+
+@in_place_transformation
 def strip_annotations(graph):
-    """Strips all the annotations from a BEL graph
+    """Strip all the annotations from a BEL graph.
 
     :param pybel.BELGraph graph: A BEL graph
     """
     for u, v, k in graph.edges_iter(keys=True):
-        if ANNOTATIONS in graph.edge[u][v][k]:
-            del graph.edge[u][v][k][ANNOTATIONS]
+        if ANNOTATIONS in graph[u][v][k]:
+            del graph[u][v][k][ANNOTATIONS]
+
+
+@in_place_transformation
+def add_annotation_value(graph, annotation, value):
+    """Add the given annotation/value pair to all qualified edges.
+
+    :param pybel.BELGraph graph:
+    :param str annotation:
+    :param str value:
+    """
+    if annotation not in graph.defined_annotation_keywords:
+        raise ValueError('annotation not defined: {}'.format(annotation))
+
+    for u, v, k in graph.edges(keys=True):
+        if ANNOTATIONS not in graph[u][v][k]:
+            continue
+
+        graph[u][v][k][ANNOTATIONS][annotation][value] = True
+
+
+@in_place_transformation
+def remove_annotation_value(graph, annotation, value):
+    """Remove the given annotation/value pair to all qualified edges.
+
+    :param pybel.BELGraph graph:
+    :param str annotation:
+    :param str value:
+    """
+    if annotation not in graph.defined_annotation_keywords:
+        log.warning('annotation was not defined: %s', annotation)
+        return
+
+    for u, v, k in graph.edges(data=True):
+        if ANNOTATIONS not in graph[u][v][k]:
+            continue
+
+        if annotation not in graph[u][v][k][ANNOTATIONS][annotation]:
+            continue
+
+        if value not in graph[u][v][k][ANNOTATIONS][annotation][value]:
+            continue
+
+        del graph[u][v][k][ANNOTATIONS][annotation][value]
