422e48c4bba908da161ac0281c3f6b998ccd9e13
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-06-26 16:27:00 +0200

    Add more star imports
    [skip ci]

--- src/pybel/struct/mutation/expansion/__init__.py
+++ src/pybel/struct/mutation/expansion/__init__.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from . import upstream
+from . import neighborhood, upstream
+from .neighborhood import *
 from .upstream import *
 
-__all__ = upstream.__all__
+__all__ = (
+        neighborhood.__all__ +
+        upstream.__all__
+)
