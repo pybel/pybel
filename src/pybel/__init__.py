985eb4413af24220628552dea54b87351ba2fb14
Charles Tapley Hoyt <cthoyt@wlan-218.scai.fraunhofer.de>
2018-06-26 17:09:26 +0200

    Bump version: 0.11.7-dev → 0.11.7

--- src/pybel/__init__.py
+++ src/pybel/__init__.py
@@ -108,7 +108,7 @@ __all__ = (
     examples.__all__
 )
 
-__version__ = '0.11.7-dev'
+__version__ = '0.11.7'
 
 __title__ = 'PyBEL'
 __description__ = 'Parsing, validation, compilation, and data exchange of Biological Expression Language (BEL)'
