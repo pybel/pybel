c63f3bb7eca68fcec6860e58bfc5a981101663c5
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-07-20 00:13:43 +0200

    Add node and edge predicate builders

--- src/pybel/struct/filters/edge_predicate_builders.py
+++ src/pybel/struct/filters/edge_predicate_builders.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
+from six import string_types
 
-from .edge_predicates import edge_predicate, keep_edge_permissive
-from ...constants import ANNOTATIONS, CAUSAL_RELATIONS, RELATION
+from .edge_predicates import edge_predicate, has_authors, has_pubmed, keep_edge_permissive
+from ...constants import ANNOTATIONS, CAUSAL_RELATIONS, CITATION, CITATION_AUTHORS, CITATION_REFERENCE, RELATION
+from ...utils import subdict_matches
 
 __all__ = [
     'build_annotation_dict_all_filter',
@@ -10,6 +12,9 @@ __all__ = [
     'build_upstream_edge_predicate',
     'build_downstream_edge_predicate',
     'build_relation_predicate',
+    'build_edge_data_filter',
+    'build_pmid_inclusion_filter',
+    'build_author_inclusion_filter',
 ]
 
 
@@ -137,3 +142,103 @@ def build_relation_predicate(relation):
         return data[RELATION] == relation
 
     return is_relation
+
+
+def build_edge_data_filter(annotations, partial_match=True):
+    """Build a filter that keeps edges whose data dictionaries are super-dictionaries to the given dictionary.
+
+    :param dict annotations: The annotation query dict to match
+    :param bool partial_match: Should the query values be used as partial or exact matches? Defaults to :code:`True`.
+    :return: An edge predicate
+    :rtype: (pybel.BELGraph, tuple, tuple, int) -> bool
+    """
+
+    @edge_predicate
+    def annotation_dict_filter(data):
+        """Pass for edges with the given dictionary as a sub-dictionary.
+
+        :param dict data: The edge data dictionary
+        :rtype: bool
+        """
+        return subdict_matches(data, annotations, partial_match=partial_match)
+
+    return annotation_dict_filter
+
+
+def build_pmid_inclusion_filter(pmid):
+    """Pass for edges with citations whose references are one of the given PubMed identifiers.
+
+    :param pmid: A PubMed identifier or list of PubMed identifiers to filter for
+    :type pmid: str or iter[str]
+    :return: An edge predicate
+    :rtype: (pybel.BELGraph, tuple, tuple, int) -> bool
+    """
+
+    if isinstance(pmid, string_types):
+        @edge_predicate
+        def pmid_inclusion_filter(data):
+            """Only passes for edges with PubMed citations matching the contained PubMed identifier
+
+            :param dict data: The edge data dictionary
+            :return: If the edge has a PubMed citation with the contained PubMed identifier
+            :rtype: bool
+            """
+            return has_pubmed(data) and data[CITATION][CITATION_REFERENCE] == pmid
+
+        return pmid_inclusion_filter
+
+    else:
+        pmids = set(pmid)
+
+        @edge_predicate
+        def pmid_inclusion_filter(data):
+            """Only passes for edges with PubMed citations matching one of the contained PubMed identifiers
+
+            :param dict data: The edge data dictionary
+            :return: If the edge has a PubMed citation with one of the contained PubMed identifiers
+            :rtype: bool
+            """
+            return has_pubmed(data) and data[CITATION][CITATION_REFERENCE] in pmids
+
+        return pmid_inclusion_filter
+
+
+def build_author_inclusion_filter(author):
+    """Build an edge predicate that only passes for edges with author information that matches one of the given authors.
+
+    :param author: An author or list of authors
+    :type author: str or iter[str]
+    :return: An edge predicate
+    :rtype: (pybel.BELGraph, tuple, tuple, int) -> bool
+    """
+    if isinstance(author, string_types):
+
+        @edge_predicate
+        def author_filter(data):
+            """Pass for edges with citations with an author that matches the contained author.
+
+            :param dict data: The edge data dictionary
+            :return: If the edge has a citation with an author that matches the the contained author
+            :rtype: bool
+            """
+            return has_authors(data) and author in data[CITATION][CITATION_AUTHORS]
+
+        return author_filter
+
+    else:
+        authors = set(author)
+
+        @edge_predicate
+        def author_filter(data):
+            """Pass for edges with citations with an author that matches one or more of the contained authors.
+
+            :param dict data: The edge data dictionary
+            :return: If the edge has a citation with an author that matches the the contained author
+            :rtype: bool
+            """
+            return has_authors(data) and any(
+                a in data[CITATION][CITATION_AUTHORS]
+                for a in authors
+            )
+
+        return author_filter
--- src/pybel/struct/filters/node_predicate_builders.py
+++ src/pybel/struct/filters/node_predicate_builders.py
@@ -6,11 +6,14 @@ from collections import Iterable
 
 from six import string_types
 
-from ...constants import FUNCTION
+from ...constants import FUNCTION, NAME
 
 __all__ = [
     'function_inclusion_filter_builder',
     'data_missing_key_builder',
+    'build_node_data_search',
+    'build_node_key_search',
+    'build_node_name_search',
 ]
 
 
@@ -83,3 +86,60 @@ def data_missing_key_builder(key):
         return key not in graph.node[node]
 
     return data_does_not_contain_key
+
+
+def build_node_data_search(key, data_filter):
+    """Pass for nodes who have the given key in their data dictionaries and whose associated values pass the given
+    filter function.
+
+    :param str key: The node data dictionary key to check
+    :param data_filter: The filter to apply to the node data dictionary
+    :type data_filter: (dict) -> bool
+    :return: A node predicate
+    :rtype: (pybel.BELGraph, tuple) -> bool
+    """
+
+    def node_data_filter(graph, node):
+        """Pass if the given node has a given data annotated and passes the contained filter.
+
+        :type graph: pybel.BELGraph
+        :type node: tuple
+        :return: If the node has the contained key in its data dictionary and passes the contained filter
+        :rtype: bool
+        """
+        data = graph.node[node]
+        return key in data and data_filter(data[key])
+
+    return node_data_filter
+
+
+def build_node_key_search(query, key):
+    """Build a node filter that only passes for nodes whose values for the given key are superstrings of the query
+    string(s).
+
+    :param query: The query string or strings to check if they're in the node name
+    :type query: str or iter[str]
+    :param str key: The key for the node data dictionary. Should refer only to entries that have str values
+    :return: A node predicate
+    :rtype: (pybel.BELGraph, tuple) -> bool
+    """
+    if isinstance(query, string_types):
+        return build_node_data_search(key, lambda s: query.lower() in s.lower())
+
+    if isinstance(query, Iterable):
+        return build_node_data_search(key, lambda s: any(q.lower() in s.lower() for q in query))
+
+    raise TypeError('query is wrong type: %s', query)
+
+
+def build_node_name_search(query):
+    """Search nodes' names.
+
+    Is a thin wrapper around :func:`build_node_key_search` with :data:`pybel.constants.NAME`
+
+    :param query: The query string or strings to check if they're in the node name
+    :type query: str or iter[str]
+    :return: A node predicate
+    :rtype: (pybel.BELGraph, tuple) -> bool
+    """
+    return build_node_key_search(query, NAME)
