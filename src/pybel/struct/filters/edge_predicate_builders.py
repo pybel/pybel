1745098492667ae00008582592385e0ed4ac3900
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-07-21 16:17:16 +0200

    Update documentatoon
    [skip ci]

--- src/pybel/struct/filters/edge_predicate_builders.py
+++ src/pybel/struct/filters/edge_predicate_builders.py
@@ -42,8 +42,8 @@ def _annotation_dict_all_filter(data, query):
 
 
 def build_annotation_dict_all_filter(annotations):
-    """Builds a filter that keeps edges whose data dictionaries's annotations entry are super-dictionaries to the given
-    dictionary.
+    """Build an edge predicate that passes for edges whose data dictionaries's annotations entry are super-dictionaries
+    to the given dictionary.
 
     If no annotations are given, will always evaluate to true.
 
@@ -85,10 +85,9 @@ def _annotation_dict_any_filter(data, query):
 
 
 def build_annotation_dict_any_filter(annotations):
-    """Builds a filter that keeps edges whose data dictionaries's annotations entry contain any match to
-    the target dictionary.
+    """Build an edge predicate that passes for edges whose data dictionaries match the given dictionary.
 
-    If no annotations are given, will always evaluate to true.
+    If the given dictionary is empty, will always evaluate to true.
 
     :param dict annotations: The annotation query dict to match
     :rtype: (pybel.BELGraph, tuple, tuple, int) -> bool
@@ -109,6 +108,13 @@ def build_annotation_dict_any_filter(annotations):
 
 
 def build_upstream_edge_predicate(nodes):
+    """Build an edge predicate that pass for relations for which one of the given nodes is the object.
+
+    :param iter[tuple] nodes: An iterable of PyBEL node tuples
+    :rtype: (pybel.BELGraph, tuple, tuple, int) -> bool
+    """
+    nodes = set(nodes)
+
     def upstream_filter(graph, u, v, k):
         """Pass for relations for which one of the given nodes is the object.
 
@@ -124,6 +130,13 @@ def build_upstream_edge_predicate(nodes):
 
 
 def build_downstream_edge_predicate(nodes):
+    """Build an edge predicate that passes for edges for which one of the given nodes is the subject.
+
+    :param iter[tuple] nodes: An iterable of PyBEL node tuples
+    :rtype: (pybel.BELGraph, tuple, tuple, int) -> bool
+    """
+    nodes = set(nodes)
+
     def downstream_filter(graph, u, v, k):
         """Pass for relations for which one of the given nodes is the subject.
 
@@ -139,7 +152,7 @@ def build_downstream_edge_predicate(nodes):
 
 
 def build_relation_predicate(relation):
-    """Build an edge predicate that matches edges with the given relation
+    """Build an edge predicate that passes for edges with the given relation.
 
     :param str relation: A relation string
     :rtype: (pybel.BELGraph, tuple, tuple, int) -> bool
@@ -159,34 +172,31 @@ def build_relation_predicate(relation):
     return relation_predicate
 
 
-def build_pmid_inclusion_filter(pmid):
-    """Pass for edges with citations whose references are one of the given PubMed identifiers.
+def build_pmid_inclusion_filter(pmids):
+    """Build an edge predicate that passes for edges with citations from the given PubMed identifier(s).
 
-    :param pmid: A PubMed identifier or list of PubMed identifiers to filter for
-    :type pmid: str or iter[str]
+    :param pmids: A PubMed identifier or list of PubMed identifiers to filter for
+    :type pmids: str or iter[str]
     :return: An edge predicate
     :rtype: (pybel.BELGraph, tuple, tuple, int) -> bool
     """
-
-    if isinstance(pmid, string_types):
+    if isinstance(pmids, string_types):
         @edge_predicate
         def pmid_inclusion_filter(data):
-            """Only passes for edges with PubMed citations matching the contained PubMed identifier
+            """Pass for edges with PubMed citations matching the contained PubMed identifier.
 
             :param dict data: The edge data dictionary
             :return: If the edge has a PubMed citation with the contained PubMed identifier
             :rtype: bool
             """
-            return has_pubmed(data) and data[CITATION][CITATION_REFERENCE] == pmid
-
-        return pmid_inclusion_filter
+            return has_pubmed(data) and data[CITATION][CITATION_REFERENCE] == pmids
 
     else:
-        pmids = set(pmid)
+        pmids = set(pmids)
 
         @edge_predicate
         def pmid_inclusion_filter(data):
-            """Only passes for edges with PubMed citations matching one of the contained PubMed identifiers
+            """Pass for edges with PubMed citations matching one of the contained PubMed identifiers.
 
             :param dict data: The edge data dictionary
             :return: If the edge has a PubMed citation with one of the contained PubMed identifiers
@@ -194,19 +204,18 @@ def build_pmid_inclusion_filter(pmid):
             """
             return has_pubmed(data) and data[CITATION][CITATION_REFERENCE] in pmids
 
-        return pmid_inclusion_filter
+    return pmid_inclusion_filter
 
 
-def build_author_inclusion_filter(author):
-    """Build an edge predicate that only passes for edges with author information that matches one of the given authors.
+def build_author_inclusion_filter(authors):
+    """Build an edge predicate that passes for edges with citations written by the given author(s).
 
-    :param author: An author or list of authors
-    :type author: str or iter[str]
+    :param authors: An author or list of authors
+    :type authors: str or iter[str]
     :return: An edge predicate
     :rtype: (pybel.BELGraph, tuple, tuple, int) -> bool
     """
-    if isinstance(author, string_types):
-
+    if isinstance(authors, string_types):
         @edge_predicate
         def author_filter(data):
             """Pass for edges with citations with an author that matches the contained author.
@@ -215,12 +224,10 @@ def build_author_inclusion_filter(author):
             :return: If the edge has a citation with an author that matches the the contained author
             :rtype: bool
             """
-            return has_authors(data) and author in data[CITATION][CITATION_AUTHORS]
-
-        return author_filter
+            return has_authors(data) and authors in data[CITATION][CITATION_AUTHORS]
 
     else:
-        authors = set(author)
+        authors = set(authors)
 
         @edge_predicate
         def author_filter(data):
@@ -231,8 +238,8 @@ def build_author_inclusion_filter(author):
             :rtype: bool
             """
             return has_authors(data) and any(
-                a in data[CITATION][CITATION_AUTHORS]
-                for a in authors
+                author in data[CITATION][CITATION_AUTHORS]
+                for author in authors
             )
 
-        return author_filter
+    return author_filter
