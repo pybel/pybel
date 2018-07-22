cbf0b33c4bd022d93ca8581a4365a4bf2fc50e30
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-07-22 18:12:11 +0200

    Add function for getting random node

--- src/pybel/struct/mutation/induction/random_subgraph.py
+++ src/pybel/struct/mutation/induction/random_subgraph.py
@@ -2,11 +2,15 @@
 
 """Functions for inducing random sub-graphs."""
 
-from random import choice, shuffle
+import bisect
+import logging
+import random
 
 __all__ = [
     'get_graph_with_random_edges',
 ]
+
+log = logging.getLogger(__name__)
 
 
 def _random_edge_iterator(graph, n_edges):
@@ -17,11 +21,11 @@ def _random_edge_iterator(graph, n_edges):
     :rtype: iter[tuple[tuple,tuple,int,dict]]
     """
     universe_edges = graph.edges()
-    shuffle(universe_edges)
+    random.shuffle(universe_edges)
 
     for u, v in universe_edges[:n_edges]:
         keys = list(graph[u][v])
-        k = choice(keys)
+        k = random.choice(keys)
         yield u, v, k, graph[u][v][k]
 
 
@@ -44,3 +48,46 @@ def get_graph_with_random_edges(graph, n_edges):
     )
 
     return result
+
+
+class WeightedRandomGenerator(object):
+    """A weighted random nmber generator,"""
+
+    def __init__(self, weights):
+        self.totals = []
+        weight_total = 0
+
+        for weight in weights:
+            weight_total += weight
+            self.totals.append(weight_total)
+
+    def next(self):
+        """Get a random index."""
+        rnd = random.random() * self.totals[-1]
+        return bisect.bisect_right(self.totals, rnd)
+
+
+def get_random_node(graph, node_blacklist):
+    """Choose a node from the graph with probabilities based on their degrees
+
+    :type graph: networkx.Graph
+    :param set[tuple] node_blacklist: Nodes to filter out
+    :rtype: Optional[tuple]
+    """
+    try:
+        nodes, degrees = zip(*(
+            (node, degree)
+            for node, degree in graph.degree_iter()
+            if node not in node_blacklist
+        ))
+    except ValueError:  # something wrong with graph, probably no elements in graph.degree_iter
+        return
+
+    normalized_degrees = [
+        degree / (2 * graph.number_of_edges())
+        for degree in degrees
+    ]
+
+    wrg = WeightedRandomGenerator(normalized_degrees)
+
+    return nodes[wrg.next()]
--- tests/test_struct/test_transformations/test_random.py
+++ tests/test_struct/test_transformations/test_random.py
@@ -4,20 +4,48 @@
 
 import random
 import unittest
+from collections import Counter
 
-from pybel.struct.mutation.induction.random_subgraph import get_graph_with_random_edges
+import networkx as nx
+
+from pybel.struct.mutation.induction.random_subgraph import get_graph_with_random_edges, get_random_node
 from pybel.testing.generate import generate_random_graph
 
 
 class TestRandom(unittest.TestCase):
     """Test random graph induction functions."""
 
-    def test_random_edges(self):
-        """Test getting a graph by random edges."""
+    def setUp(self):
+        """Set the random seed before each test."""
         random.seed(127)  # love that number
 
+    def test_random_edges(self):
+        """Test getting a graph by random edges."""
         n_nodes, n_edges, n_sample_edges = 15, 80, 40
         graph = generate_random_graph(n_nodes=n_nodes, n_edges=n_edges)
 
         subgraph = get_graph_with_random_edges(graph, n_edges=n_sample_edges)
         self.assertEqual(n_sample_edges, subgraph.number_of_edges())
+
+    def test_random_nodes(self):
+        """Test getting random nodes."""
+        graph = nx.MultiDiGraph()
+
+        graph.add_edge(1, 2)
+        graph.add_edge(1, 3)
+        graph.add_edge(1, 4)
+        graph.add_edge(1, 5)
+
+        n = 30000
+        r = Counter(
+            get_random_node(graph, set())
+            for _ in range(n)
+        )
+
+        print(r)
+
+        self.assertAlmostEqual(4 / 8, r[1] / n, places=2)
+        self.assertAlmostEqual(1 / 8, r[2] / n, places=2)
+        self.assertAlmostEqual(1 / 8, r[3] / n, places=2)
+        self.assertAlmostEqual(1 / 8, r[4] / n, places=2)
+        self.assertAlmostEqual(1 / 8, r[5] / n, places=2)
