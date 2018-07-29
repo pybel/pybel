e930c85e8f8f6a40cc237f01ed22ccfb1cc56ee2
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-07-29 12:33:20 +0200

    Update CLI
    - remove weird debug level setting statements
    - remove url and database as arguments from convert command

--- src/pybel/cli.py
+++ src/pybel/cli.py
@@ -23,10 +23,10 @@ from click_plugins import with_plugins
 
 from .canonicalize import to_bel
 from .constants import get_cache_connection
-from .io import from_lines, from_url, to_csv, to_graphml, to_gsea, to_json_file, to_neo4j, to_pickle, to_sif
+from .io import from_lines, to_csv, to_graphml, to_gsea, to_json_file, to_neo4j, to_pickle, to_sif
 from .io.web import _get_host
 from .manager import Manager, defaults
-from .manager.database_io import from_database, to_database
+from .manager.database_io import to_database
 from .manager.models import Base, Edge, Namespace
 
 log = logging.getLogger(__name__)
@@ -54,9 +54,7 @@ def main():
 
 @main.command()
 @click.option('-p', '--path', type=click.File('r'), default=sys.stdin, help='Input BEL file file path')
-@click.option('--url', help='Input BEL file URL')
 @connection_option
-@click.option('--database-name', help='Input network name from database')
 @click.option('--csv', type=click.File('w'), help='Path to output a CSV file.')
 @click.option('--sif', type=click.File('w'), help='Path to output an SIF file.')
 @click.option('--gsea', type=click.File('w'), help='Path to output a GRP file for gene set enrichment analysis')
@@ -74,46 +72,23 @@ def main():
 @click.option('--no-identifier-validation', is_flag=True, help='Turn off identifier validation')
 @click.option('--no-citation-clearing', is_flag=True, help='Turn off citation clearing')
 @click.option('-r', '--required-annotations', multiple=True, help='Specify multiple required annotations')
-@click.option('-v', '--debug', count=True)
-def convert(path, url, connection, database_name, csv, sif, gsea, graphml, json, pickle, bel, neo, neo_context, store,
-            allow_naked_names, allow_nested, allow_unqualified_translocations, no_identifier_validation,
-            no_citation_clearing, debug, required_annotations):
+def convert(path, connection, csv, sif, gsea, graphml, json, pickle, bel, neo, neo_context, store, allow_naked_names,
+            allow_nested, allow_unqualified_translocations, no_identifier_validation, no_citation_clearing,
+            required_annotations):
     """Convert BEL."""
-    if debug == 1:
-        log.setLevel(logging.INFO)
-        logging.basicConfig(level=logging.INFO, format='%(name)s:%(levelname)s - %(message)s')
-    elif debug == 2:
-        log.setLevel(logging.DEBUG)
-        logging.basicConfig(level=logging.DEBUG, format='%(name)s:%(levelname)s - %(message)s')
-
     manager = Manager(connection=connection)
 
-    if database_name:
-        g = from_database(database_name, manager=manager)
-
-    elif url:
-        g = from_url(
-            url,
-            manager=manager,
-            allow_nested=allow_nested,
-            allow_naked_names=allow_naked_names,
-            allow_unqualified_translocations=allow_unqualified_translocations,
-            citation_clearing=(not no_citation_clearing),
-            no_identifier_validation=no_identifier_validation,
-            required_annotations=required_annotations,
-        )
-
-    else:
-        g = from_lines(
-            path,
-            manager=manager,
-            allow_nested=allow_nested,
-            allow_naked_names=allow_naked_names,
-            allow_unqualified_translocations=allow_unqualified_translocations,
-            citation_clearing=(not no_citation_clearing),
-            required_annotations=required_annotations,
-            use_tqdm=True,
-        )
+    g = from_lines(
+        path,
+        manager=manager,
+        allow_nested=allow_nested,
+        allow_naked_names=allow_naked_names,
+        allow_unqualified_translocations=allow_unqualified_translocations,
+        citation_clearing=(not no_citation_clearing),
+        required_annotations=required_annotations,
+        no_identifier_validation=no_identifier_validation,
+        use_tqdm=True,
+    )
 
     if csv:
         log.info('Outputting CSV to %s', csv)
@@ -200,11 +175,9 @@ def manage(ctx, connection):
 
 
 @manage.command()
-@click.option('-v', '--debug', count=True)
 @click.pass_obj
-def setup(manager, debug):
-    """Create the cache if it doesn't exist"""
-    log.setLevel(int(5 * debug ** 2 / 2 - 25 * debug / 2 + 20))
+def setup(manager):
+    """Create the database, if it doesn't exist."""
     manager.create_all()
 
 
@@ -228,23 +201,17 @@ def annotations():
 
 
 @namespace.command()
-@click.option('-v', '--debug', count=True)
 @click.pass_obj
-def ensure(manager, debug):
-    """Set up default cache with default definitions"""
-    log.setLevel(int(5 * debug ** 2 / 2 - 25 * debug / 2 + 20))
-
+def ensure(manager):
+    """Set up default cache with default definitions."""
     for url in defaults.fraunhofer_namespaces:
         manager.ensure_namespace(url)
 
 
 @annotations.command()
-@click.option('-v', '--debug', count=True)
 @click.pass_obj
-def ensure(manager, debug):
-    """Set up default cache with default annotations"""
-    log.setLevel(int(5 * debug ** 2 / 2 - 25 * debug / 2 + 20))
-
+def ensure(manager):
+    """Set up default cache with default annotations."""
     for url in defaults.fraunhofer_annotations:
         manager.ensure_annotation(url)
 
