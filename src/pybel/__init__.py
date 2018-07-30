41130d9089ba5cad965db6027f89e18ae90cc32f
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-07-30 11:01:14 +0200

    Add get_version to top level

--- src/pybel/__init__.py
+++ src/pybel/__init__.py
@@ -91,14 +91,17 @@ from .manager import cache_manager, database_io
 from .manager.cache_manager import *
 from .manager.database_io import *
 from .struct import *
+from .utils import get_version
 
 __all__ = (
-    struct.__all__ +
-    io.__all__ +
-    canonicalize.__all__ +
-    database_io.__all__ +
-    cache_manager.__all__ +
-    examples.__all__
+        struct.__all__ +
+        io.__all__ +
+        canonicalize.__all__ +
+        database_io.__all__ +
+        cache_manager.__all__ +
+        examples.__all__ + [
+            'get_version',
+        ]
 )
 
 __version__ = '0.11.11-dev'
