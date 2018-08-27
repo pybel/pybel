a9b06052d93e4a9c27eadc9e5ff59eee9da80de9
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-08-27 13:04:39 +0200

    Add relabel-inplace
    Doesn't need testing since just changes the way nodes are added. Can be removed once the tupleectomy happens

--- src/pybel/struct/utils.py
+++ src/pybel/struct/utils.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
+import networkx as nx
+
 __all__ = [
     'update_metadata',
     'update_node_helper',
     'ensure_node_from_universe',
+    'relabel_inplace',
 ]
 
 
@@ -49,3 +52,44 @@ def ensure_node_from_universe(source, target, node):
     if node not in target:
         target.add_node(node)
         target._node[node] = source._node[node]
+
+
+def relabel_inplace(G, mapping):  # borrowed from NX
+    old_labels = set(mapping.keys())
+    new_labels = set(mapping.values())
+    if len(old_labels & new_labels) > 0:
+        # labels sets overlap
+        # can we topological sort and still do the relabeling?
+        D = nx.DiGraph(list(mapping.items()))
+        D.remove_edges_from(nx.selfloop_edges(D))
+        try:
+            nodes = reversed(list(nx.topological_sort(D)))
+        except nx.NetworkXUnfeasible:
+            raise nx.NetworkXUnfeasible('The node label sets are overlapping '
+                                        'and no ordering can resolve the '
+                                        'mapping. Use copy=True.')
+    else:
+        # non-overlapping label sets
+        nodes = old_labels
+
+    for old in nodes:
+        try:
+            new = mapping[old]
+        except KeyError:
+            continue
+        if new == old:
+            continue
+        try:
+            G.add_node(new)
+            G._node[new] = G.nodes[old]  # THIS WAS CHANGED
+        except KeyError:
+            raise KeyError("Node %s is not in the graph" % old)
+        new_edges = [(new, new if old == target else target, key, data)
+                     for (_, target, key, data)
+                     in G.edges(old, data=True, keys=True)]
+        new_edges += [(new if old == source else source, new, key, data)
+                      for (source, _, key, data)
+                      in G.in_edges(old, data=True, keys=True)]
+        G.remove_node(old)
+        G.add_edges_from(new_edges)
+    return G
