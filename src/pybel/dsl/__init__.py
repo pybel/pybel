60e66bc841330ff3c940ba4bc32cc47bc3a9b8da
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-08-28 11:28:38 +0200

    Update organization of DSL

--- src/pybel/dsl/__init__.py
+++ src/pybel/dsl/__init__.py
@@ -2,14 +2,18 @@
 
 """An internal domain-specific language (DSL) for BEL."""
 
-from . import edges, nodes
+from . import constants, edges, node_classes, nodes
+from .constants import *
 from .edges import *
 from .exc import PyBELDSLException
+from .node_classes import *
 from .nodes import *
 from .utils import entity
 
 __all__ = (
-    nodes.__all__ +
-    edges.__all__ +
-    ['PyBELDSLException', 'entity']
+        node_classes.__all__ +
+        constants.__all__ +
+        nodes.__all__ +
+        edges.__all__ +
+        ['PyBELDSLException', 'entity']
 )
