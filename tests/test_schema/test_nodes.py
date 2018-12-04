7e1a35fb2d1831c18029518ac4d71e3706f1887e
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-12-04 14:29:39 +0100

    Update node schema and testing
    Co-Authored-By: Simon Müller <s.mueller1995@gmail.com>

--- tests/test_schema/test_nodes.py
+++ tests/test_schema/test_nodes.py
@@ -1,11 +1,31 @@
+# -*- coding: utf-8 -*-
+
+"""Unit tests for validating DSL nodes."""
+
 import unittest
 
-from pybel import schema as pbs
-from pybel.dsl import Protein
+from pybel.constants import NAME, NAMESPACE
+from pybel.dsl import Protein, ProteinModification
+from pybel.schema import validate_node
 
 n1 = Protein('HGNC', 'YFG')
+n2 = Protein('HGNC', 'YFG', variants=ProteinModification('Ph'))
 
 
 class TestNodeSchema(unittest.TestCase):
+    """Tests for the PyBEL node validator."""
+
     def test_simple(self):
-        self.assertTrue(pbs.validate_node(n1))
+        """Test validating simple namespace/name nodes."""
+        self.assertTrue(validate_node(n1))
+
+    def test_simple_fail(self):
+        """Test validating simple namespace/name nodes."""
+        self.assertFalse(validate_node({
+            NAMESPACE: 'HGNC',
+            NAME: 'YFG',
+        }))
+
+    def test_protein_with_modification(self):
+        """Test validating a protein with modifications."""
+        self.assertTrue(validate_node(n2))
