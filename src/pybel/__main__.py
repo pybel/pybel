de146a4a0fa0ce227d9feaaffdc95bba05344995
Charles Tapley Hoyt <cthoyt@gmail.com>
2019-03-20 11:44:09 +0100

    Update documentation
    [skip ci]

--- src/pybel/__main__.py
+++ src/pybel/__main__.py
@@ -5,7 +5,6 @@
 Why does this file exist, and why __main__? For more info, read:
 
  - https://www.python.org/dev/peps/pep-0338/
- - https://docs.python.org/2/using/cmdline.html#cmdoption-m
  - https://docs.python.org/3/using/cmdline.html#cmdoption-m
 """
 
