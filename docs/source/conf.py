3ce7da81295d0c6ca5ff0cb3163625fef5a262c0
Charles Tapley Hoyt <cthoyt@gmail.com>
2019-04-25 19:43:47 +0200

    Add PyBEL logo for the docs
    [skip ci]

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -152,7 +152,7 @@ html_theme = 'sphinx_rtd_theme'
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 #
-# html_logo = None
+html_logo = 'PyBEL-square-100.png'
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
