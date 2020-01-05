f0b6241ef70f1ac08aa9231279f9a1b5e5c8d1f8
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-01-05 21:21:31 +0100

    Update graphml.py

--- src/pybel/io/graphml.py
+++ src/pybel/io/graphml.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-"""Conversion functions for BEL graphs with GraphML. More information at https://en.wikipedia.org/wiki/GraphML."""
+"""Conversion functions for BEL graphs with `GraphML <https://en.wikipedia.org/wiki/GraphML>`_."""
+
 from typing import BinaryIO, Optional, Union
 
 import networkx as nx
@@ -15,29 +16,29 @@ __all__ = [
 
 
 def to_graphml(graph: BELGraph, path: Union[str, BinaryIO], schema: Optional[str] = None) -> None:
-    """Write this graph to GraphML XML file using :func:`networkx.write_graphml`.
+    """Write a graph to a GraphML XML file using :func:`networkx.write_graphml`.
 
     :param graph: BEL Graph
     :param path: Path to the new exported file
-    :param schema: Type of export
+    :param schema: Type of export. Currently supported: "simple" and "umbrella".
 
     The .graphml file extension is suggested so Cytoscape can recognize it.
     By default, this function exports using the PyBEL schema of including modifier information into the edges.
     As an alternative, this function can also distinguish between
     """
     if schema is None or schema == 'simple':
-        return _to_graphml_simple(graph, path)
-    elif schema == 'belframework-counterintuitive':
-        return _to_graphml_canonical(graph, path)
+        rv = _to_graphml_simple(graph)
+    elif schema == 'umbrella':
+        rv = _to_graphml_umbrella(graph)
     else:
         raise ValueError('Unhandled schema: {}'.format(schema))
+    
+    nx.write_graphml(rv, path)
 
-
-def _to_graphml_simple(graph: BELGraph, path: Union[str, BinaryIO]) -> None:
+def _to_graphml_simple(graph: BELGraph) -> nx.MultiDiGraph:
     """Convert a BEL graph to GraphML XML file using the PyBEL schema.
 
-    :param graph: BEL Graph
-    :param path: Path to the new exported file
+    :param graph: A BEL graph
     """
     rv = nx.MultiDiGraph()
 
@@ -45,36 +46,34 @@ def _to_graphml_simple(graph: BELGraph, path: Union[str, BinaryIO]) -> None:
         rv.add_node(node.as_bel(), function=node.function)
 
     for u, v, key, edge_data in graph.edges(data=True, keys=True):
+        u_key, v_key = u.as_bel(), v.as_bel()
         rv.add_edge(
-            u.as_bel(),
-            v.as_bel(),
+            u_key,
+            v_key,
+            key=key,
             interaction=edge_data[RELATION],
             bel=graph.edge_to_bel(u, v, edge_data),
-            key=key,
         )
 
-    nx.write_graphml(rv, path)
+    return rv
 
 
-def _to_graphml_canonical(graph: BELGraph, path: Union[str, BinaryIO]) -> None:
+def _to_graphml_umbrella(graph: BELGraph) -> nx.MultiDiGraph:
     """Convert a BEL graph to GraphML XML file by previously canonicalizing the nodes.
 
-    :param graph: BEL Graph
-    :param path: Path to the new exported file
+    :param graph: A BEL graph
     """
     rv = nx.MultiDiGraph()
 
-    for u, v, key, data in graph.edges(data=True, keys=True):
-        canonical_u, _, canonical_v = edge_to_tuple(u, v, data)
+    for u, v, key, edge_data in graph.edges(data=True, keys=True):
+        u_key, _, v_key = edge_to_tuple(u, v, edge_data)
 
         rv.add_edge(
-            canonical_u,
-            canonical_v,
+            u_key,
+            v_key,
             key=key,
-            **{
-                'relation': data[RELATION],
-                'bel': graph.edge_to_bel(u, v, data)
-            }
+            relation=edge_data[RELATION],
+            bel=graph.edge_to_bel(u, v, edge_data),
         )
-
-    nx.write_graphml(rv, path)
+    
+    return rv
