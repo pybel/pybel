fc18903478c1d8b51bff27408d7f0904d26020d5
daniel.domingo.fernandez <danieldomingofernandez@hotmail.com>
2020-01-05 20:45:17 +0100

    Add custom json tests

---
+++ tests/test_io/test_custom_json.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+"""Tests for the customized node-link JSON exporter (canonicalization)."""
+
+import unittest
+from tests.test_io.test_cx.examples import example_graph
+from pybel import to_canonicalized_json
+
+
+class TestExporter(unittest.TestCase):
+    """Tests for the node-link JSON exporter."""
+
+    def test_exporter_new_nodes(self):
+        """Test new nodes created."""
+        graph = example_graph.copy()
+
+        # Check original number of nodes and edges in the example BEL Graph
+        self.assertEqual(29, graph.number_of_nodes())
+        self.assertEqual(29, graph.number_of_edges())
+
+        custom_json_dict = to_canonicalized_json(graph)
+
+        # 3 new nodes are created:
+        # act(p(hgnc:MAPK1)
+        # act(p(hgnc:PTK2, pmod(Ph, Tyr, 925)), ma(kin))
+        # act(p(fus(hgnc:BCR, "?", hgnc:ABL1, "?")), ma(kin))
+        self.assertEqual(32, len(custom_json_dict['nodes']))
+        # Number of edges is maintained
+        self.assertEqual(29, len(custom_json_dict['links']))
