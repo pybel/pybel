9b3b70211f177e244e506ce0e6d9e34e14250d9a
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-01-07 15:15:14 +0100

    Add TODOs in tests

--- tests/test_io/test_umbrella_nodelink_json.py
+++ tests/test_io/test_umbrella_nodelink_json.py
@@ -3,27 +3,31 @@
 """Tests for the umbrella node-link JSON exporter."""
 
 import unittest
+
+from pybel.io.umbrella_nodelink import to_umbrella_nodelink
 from tests.test_io.test_cx.examples import example_graph
-from pybel import to_umbrella_nodelink
 
 
-class TestExporter(unittest.TestCase):
-    """Tests for the node-link JSON exporter."""
+class TestUmbrellaNodeLinkExporter(unittest.TestCase):
+    """Tests for the umbrella node-link JSON exporter."""
 
     def test_exporter_new_nodes(self):
         """Test new nodes created."""
-        graph = example_graph.copy()
-
         # Check original number of nodes and edges in the example BEL Graph
-        self.assertEqual(29, graph.number_of_nodes())
-        self.assertEqual(29, graph.number_of_edges())
+        self.assertEqual(29, example_graph.number_of_nodes())
+        self.assertEqual(29, example_graph.number_of_edges())
 
-        custom_json_dict = to_umbrella_nodelink(graph)
+        custom_json_dict = to_umbrella_nodelink(example_graph)
 
         # 3 new nodes are created:
         # act(p(hgnc:MAPK1)
         # act(p(hgnc:PTK2, pmod(Ph, Tyr, 925)), ma(kin))
         # act(p(fus(hgnc:BCR, "?", hgnc:ABL1, "?")), ma(kin))
         self.assertEqual(32, len(custom_json_dict['nodes']))
+
+        # TODO test for these nodes actually being in the nodes list
+
         # Number of edges is maintained
         self.assertEqual(29, len(custom_json_dict['links']))
+
+        # TODO test some actual edges being in the edge list
