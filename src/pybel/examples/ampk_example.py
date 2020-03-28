aeca4819d786c40c6a0779234ddd2b3e1ba0a061
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-03-28 12:15:01 +0100

    Update ampk_example.py
    [skip ci]

--- src/pybel/examples/ampk_example.py
+++ src/pybel/examples/ampk_example.py
@@ -6,14 +6,14 @@ from ..dsl import NamedComplexAbundance, Protein
 from ..struct import BELGraph
 
 ampk_graph = BELGraph()
-ampk = Protein(namespace='fplx', name='AMPK')
+ampk = NamedComplexAbundance(namespace='fplx', name='AMPK')
 
 # Alpha subunits of AMPK
-ampk_alpha = NamedComplexAbundance(namespace='fplx', name='AMPK_alpha')
-ampk_graph.add_part_of(ampk_alpha, ampk_alpha)
+ampk_alpha = Protein(namespace='fplx', name='AMPK_alpha')
+ampk_graph.add_part_of(ampk_alpha, ampk)
 
-prkaa1 = Protein(namespace='hgnc', name='prkaa1')
-prkaa2 = Protein(namespace='hgnc', name='prkaa2')
+prkaa1 = Protein(namespace='hgnc', identifier='9376', name='PRKAA1')
+prkaa2 = Protein(namespace='hgnc', identifier='9377', name='PRKAA2')
 ampk_graph.add_is_a(prkaa1, ampk_alpha)
 ampk_graph.add_is_a(prkaa2, ampk_alpha)
 
@@ -21,8 +21,8 @@ ampk_graph.add_is_a(prkaa2, ampk_alpha)
 ampk_beta = Protein(namespace='fplx', name='AMPK_beta')
 ampk_graph.add_part_of(ampk_beta, ampk)
 
-prkab1 = Protein(namespace='hgnc', name='prkab1')
-prkab2 = Protein(namespace='hgnc', name='prkab2')
+prkab1 = Protein(namespace='hgnc', identifier='9378', name='PRKAB1')
+prkab2 = Protein(namespace='hgnc', identifier='9379', name='PRKAB2')
 ampk_graph.add_is_a(prkab1, ampk_beta)
 ampk_graph.add_is_a(prkab2, ampk_beta)
 
@@ -30,16 +30,16 @@ ampk_graph.add_is_a(prkab2, ampk_beta)
 ampk_gamma = Protein(namespace='fplx', name='AMPK_gamma')
 ampk_graph.add_part_of(ampk_gamma, ampk)
 
-prkag1 = Protein(namespace='hgnc', name='prkab1')
-prkag2 = Protein(namespace='hgnc', name='prkab2')
-prkag3 = Protein(namespace='hgnc', name='prkab3')
+prkag1 = Protein(namespace='hgnc', identifier='9385', name='PRKAG1')
+prkag2 = Protein(namespace='hgnc', identifier='9386', name='PRKAG2')
+prkag3 = Protein(namespace='hgnc', identifier='9387', name='PRKAG3')
 ampk_graph.add_is_a(prkag1, ampk_gamma)
 ampk_graph.add_is_a(prkag2, ampk_gamma)
 ampk_graph.add_is_a(prkag3, ampk_gamma)
 
 mtorc1 = NamedComplexAbundance(namespace='fplx', name='mTORC1')
-mtor = Protein(namespace='hgnc', name='mtor')
-rptor = Protein(namespace='hgnc', name='rptor')
+mtor = Protein(namespace='hgnc', identifier='3942', name='MTOR')
+rptor = Protein(namespace='hgnc', identifier='30287', name='RPTOR')
 ampk_graph.add_part_of(mtor, mtorc1)
 ampk_graph.add_part_of(rptor, mtorc1)
 
