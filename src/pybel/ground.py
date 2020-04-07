a785e32334f81faf5b9b0d0083447b4c6d4ed886
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-04-07 17:02:57 +0200

    Support grounding fusions

--- src/pybel/ground.py
+++ src/pybel/ground.py
@@ -5,6 +5,10 @@ from tqdm import tqdm
 
 import pybel
 from pybel import BELGraph
+from pybel.constants import (
+    CONCEPT, FUSION, IDENTIFIER, MEMBERS, NAME, NAMESPACE, PARTNER_3P, PARTNER_5P, PRODUCTS,
+    REACTANTS,
+)
 
 __all__ = [
     'ground_nodelink',
@@ -30,23 +34,32 @@ def ground_nodelink(j) -> None:
 
     for node in tqdm(j['nodes'], desc='mapping nodes'):
         _process_concept(node, name_to_ids)
-        _process_members(node, name_to_ids, 'members')
-        _process_members(node, name_to_ids, 'reactants')
-        _process_members(node, name_to_ids, 'products')
+        _process_members(node, name_to_ids, MEMBERS)
+        _process_members(node, name_to_ids, REACTANTS)
+        _process_members(node, name_to_ids, PRODUCTS)
+        _process_fusion(node, name_to_ids)
 
 
 def _process_concept(node, name_to_ids):
-    concept = node.get('concept')
+    concept = node.get(CONCEPT)
     if concept is None:
         return
 
-    namespace = concept['namespace'] = concept['namespace'].lower()
+    namespace = concept[NAMESPACE] = concept[NAMESPACE].lower()
     if namespace in NO_NAMES:
-        concept['identifier'] = concept['name']
+        concept[IDENTIFIER] = concept[NAME]
     elif namespace in name_to_ids:
-        identifier = name_to_ids[namespace].get(concept['name'])
+        identifier = name_to_ids[namespace].get(concept[NAME])
         if identifier is not None:
-            concept['identifier'] = identifier
+            concept[IDENTIFIER] = identifier
+
+
+def _process_fusion(node, name_to_ids):
+    fusion = node.get(FUSION)
+    if not fusion:
+        return
+    _process_concept(fusion[PARTNER_3P], name_to_ids)
+    _process_concept(fusion[PARTNER_5P], name_to_ids)
 
 
 def _process_members(node, name_to_ids, key):
