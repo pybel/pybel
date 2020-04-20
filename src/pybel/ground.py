90f4efb4e8fd074230f496974268bcec0af0ae53
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-04-20 14:20:03 +0200

    Update grounder, add tests, implement for pmod
    Also added a TODO for activities that need to get normalized too

--- src/pybel/ground.py
+++ src/pybel/ground.py
@@ -6,7 +6,7 @@ import logging
 import unittest
 from typing import Tuple, Union
 
-from protmapper.uniprot_client import get_mnemonic
+from protmapper.uniprot_client import get_id_from_mnemonic, get_mnemonic
 from pyobo.extract import get_id_name_mapping, get_name_id_mapping
 from pyobo.getters import NoOboFoundry
 from pyobo.identifier_utils import normalize_prefix
@@ -14,16 +14,16 @@ from pyobo.xrefdb.sources.famplex import get_remapping
 from tqdm import tqdm
 
 from pybel.constants import (
-    CONCEPT, FUSION, IDENTIFIER, MEMBERS, NAME, NAMESPACE, PARTNER_3P, PARTNER_5P, PRODUCTS,
-    REACTANTS,
+    CONCEPT, FUSION, GMOD, IDENTIFIER, KIND, MEMBERS, NAME, NAMESPACE, PARTNER_3P, PARTNER_5P, PMOD, PRODUCTS,
+    REACTANTS, VARIANTS,
 )
 from pybel.io import from_nodelink, to_nodelink
+from pybel.language import gmod_mappings, pmod_mappings
 from pybel.struct import BELGraph
 
 __all__ = [
     'ground_nodelink',
-    'ground_graph',
-    'ground_node',
+    'ground',
 ]
 
 logger = logging.getLogger(__name__)
@@ -38,37 +38,42 @@ def _get_remapping(prefix, name) -> Union[Tuple[str, str, str], Tuple[None, None
     return _REMAPPING.get((prefix, name), (None, None, None))
 
 
-def ground_graph(graph: BELGraph) -> BELGraph:
+def ground(graph: BELGraph) -> BELGraph:
     """Ground all entities in a BEL graph."""
     j = to_nodelink(graph)
     ground_nodelink(j)
     return from_nodelink(j)
 
 
-def ground_nodelink(j) -> None:
+def ground_nodelink(graph_nodelink_dict) -> None:
     """Ground entities in a nodelink data structure."""
-    for node in tqdm(j['nodes'], desc='mapping nodes'):
-        ground_node(node)
+    for node in tqdm(graph_nodelink_dict['nodes'], desc='mapping nodes'):
+        _process_node(node)
+
+    # TODO ground entities in edges! what about all those fabulous activities?
 
 
 _UNHANDLED_NAMESPACES = set()
 
 
-def ground_node(node) -> None:
+def _process_node(node) -> None:
     """Process a node JSON object, in place."""
-    _process_concept(node)
-    _process_members(node, MEMBERS)
-    _process_members(node, REACTANTS)
-    _process_members(node, PRODUCTS)
-    _process_fusion(node)
+    if CONCEPT in node:
+        _process_concept(node[CONCEPT])
+    if VARIANTS in node:
+        _process_list(node[VARIANTS])
+    if MEMBERS in node:
+        _process_list(node[MEMBERS])
+    if REACTANTS in node:
+        _process_list(node[REACTANTS])
+    if PRODUCTS in node:
+        _process_list(node[PRODUCTS])
+    if FUSION in node:
+        _process_fusion(node[FUSION])
 
 
-def _process_concept(node) -> None:
+def _process_concept(concept) -> None:
     """Process a node JSON object."""
-    concept = node.get(CONCEPT)
-    if concept is None:
-        return
-
     namespace = concept[NAMESPACE]
     if namespace.lower() in {'text', 'fixme'}:
         return
@@ -90,6 +95,10 @@ def _process_concept(node) -> None:
 
 
 def _handle_identifier_not_name(*, concept, prefix, identifier) -> None:
+    if prefix == 'uniprot':
+        concept[NAME] = get_mnemonic(identifier)
+        return
+
     id_name_mapping = get_id_name_mapping(prefix)
     if id_name_mapping is None:
         logger.warning('could not get names for prefix %s', prefix)
@@ -119,16 +128,34 @@ def _handle_name_and_not_identifier(*, concept, prefix, name) -> None:
         concept[IDENTIFIER] = name
         return
 
+    if prefix == 'bel':
+        kind = concept.get(KIND)
+        if kind == PMOD and name in pmod_mappings:
+            # the 0th position xref is the preferred one (usually GO)
+            _mapped = pmod_mappings[name]['xrefs'][0]
+        elif kind == GMOD and name in gmod_mappings:
+            _mapped = gmod_mappings[name]['xrefs'][0]
+        else:
+            raise ValueError(f'invalid kind: {kind}')
+        concept[NAMESPACE] = _mapped[NAMESPACE]
+        concept[IDENTIFIER] = _mapped[IDENTIFIER]
+        concept[NAME] = _mapped[NAME]
+        return
+
     if prefix == 'uniprot':
-        identifier = concept.get(IDENTIFIER)
+        # assume identifier given as name
+        identifier = get_id_from_mnemonic(name)
         if identifier is not None:
-            concept[NAME] = get_mnemonic(identifier, web_fallback=True)
+            concept[IDENTIFIER] = identifier
             return
 
-        # assume identifier given as name
         mnemomic = get_mnemonic(name, web_fallback=True)
-        concept[IDENTIFIER] = name
-        concept[NAME] = mnemomic
+        if mnemomic is not None:
+            concept[IDENTIFIER] = name
+            concept[NAME] = mnemomic
+            return
+
+        logger.warning('could not interpret uniprot name: %s', name)
         return
 
     try:
@@ -148,18 +175,12 @@ def _handle_name_and_not_identifier(*, concept, prefix, name) -> None:
     concept[IDENTIFIER] = identifier
 
 
-def _process_fusion(node) -> None:
-    fusion = node.get(FUSION)
-    if not fusion:
-        return
+def _process_fusion(fusion) -> None:
     _process_concept(fusion[PARTNER_3P])
     _process_concept(fusion[PARTNER_5P])
 
 
-def _process_members(node, key) -> None:
-    members = node.get(key)
-    if not members:
-        return
+def _process_list(members) -> None:
     for member in members:
         _process_concept(member)
 
@@ -167,45 +188,146 @@ def _process_members(node, key) -> None:
 class TestGround(unittest.TestCase):
     """Test grounding."""
 
-    def _test_ground(self, expected, result):
-        ground_node(result)
-        self.assertEqual(expected, result)
+    def test_process_concept(self):
+        """Test several cases in processing concepts."""
+        r = [
+            (
+                'Normalize prefix to correct case',
+                {NAMESPACE: 'mesh', NAME: 'Neurons', IDENTIFIER: 'D009474'},
+                {NAMESPACE: 'MESH', NAME: 'Neurons', IDENTIFIER: 'D009474'},
+            ),
+            (
+                'Normalize prefix by synonym',
+                {NAMESPACE: 'mesh', NAME: 'Neurons', IDENTIFIER: 'D009474'},
+                {NAMESPACE: 'MESHA', NAME: 'Neurons', IDENTIFIER: 'D009474'},
+            ),
+            (
+                'Look up identifier by name',
+                {NAMESPACE: 'mesh', NAME: 'Neurons', IDENTIFIER: 'D009474'},
+                {NAMESPACE: 'MESH', NAME: 'Neurons'},
+            ),
+            (
+                'Lookup by UniProt identifier as name',
+                {NAMESPACE: 'uniprot', NAME: 'HUS1_HUMAN', IDENTIFIER: 'O60921'},
+                {NAMESPACE: 'UniProt', NAME: 'O60921'},
+            ),
+            (
+                'Lookup by UniProt mnemonic as name',
+                {NAMESPACE: 'uniprot', NAME: 'HUS1_HUMAN', IDENTIFIER: 'O60921'},
+                {NAMESPACE: 'UniProt', NAME: 'HUS1_HUMAN'},
+            ),
+            (
+                'Rewrite wrong UniProt name',
+                {NAMESPACE: 'uniprot', NAME: 'HUS1_HUMAN', IDENTIFIER: 'O60921'},
+                {NAMESPACE: 'UniProt', NAME: 'WRONG!!!!', IDENTIFIER: 'O60921'},
+            ),
+            (
+                'Overwrite name by identifier',
+                {NAMESPACE: 'mesh', NAME: 'Neurons', IDENTIFIER: 'D009474'},
+                {NAMESPACE: 'MESH', NAME: 'Nonsense name!', IDENTIFIER: 'D009474'},
+            ),
+            (
+                'Remap example 1',
+                {NAMESPACE: 'fplx', NAME: 'TAP', IDENTIFIER: 'TAP'},
+                {NAMESPACE: 'SFAM', NAME: 'TAP Family'},
+            ),
+            (
+                'Remap example 2',
+                {NAMESPACE: 'fplx', NAME: 'Gamma_secretase', IDENTIFIER: 'Gamma_secretase'},
+                {NAMESPACE: 'SCOMP', NAME: 'gamma Secretase Complex'},
+            ),
+        ]
+        for name, expected, result in r:
+            with self.subTest(name=name):
+                _process_concept(result)
+                self.assertEqual(expected, result)
 
-    def test_normalize_prefix_case(self):
-        """Test that the prefix is normalized to the correct case."""
-        self._test_ground(
-            {NAMESPACE: 'mesh', NAME: 'Neurons', IDENTIFIER: 'D009474'},
-            {NAMESPACE: 'MESH', NAME: 'Neurons', IDENTIFIER: 'D009474'}
-        )
+    def test_process_nodes(self):
+        """Test several cases dealing with full nodes."""
+        r = [
+            (
+                'Look up identifier of member by name',
+                {MEMBERS: [{NAMESPACE: 'mesh', NAME: 'Neurons', IDENTIFIER: 'D009474'}]},
+                {MEMBERS: [{NAMESPACE: 'MESH', NAME: 'Neurons'}]},
+            ),
+            (
+                'Look up identifier of member by name AND name of complex',
+                {
+                    CONCEPT: {NAMESPACE: 'complexportal', NAME: 'Checkpoint clamp complex', IDENTIFIER: 'CPX-1829'},
+                    MEMBERS: [
+                        {NAMESPACE: 'uniprot', NAME: 'HUS1_HUMAN', IDENTIFIER: 'O60921'},
+                        {NAMESPACE: 'uniprot', NAME: 'RAD9A_HUMAN', IDENTIFIER: 'Q99638'},
+                        {NAMESPACE: 'uniprot', NAME: 'RAD1_HUMAN', IDENTIFIER: 'O60671'},
+                    ]
+                },
+                {
+                    CONCEPT: {NAMESPACE: 'complexportal', NAME: 'Checkpoint clamp complex'},
+                    MEMBERS: [
+                        {NAMESPACE: 'uniprot', NAME: 'HUS1_HUMAN'},
+                        {NAMESPACE: 'uniprot', NAME: 'RAD9A_HUMAN'},
+                        {NAMESPACE: 'uniprot', NAME: 'RAD1_HUMAN'},
+                    ]
+                },
+            ),
+            (
+                'Normalize pmod by name',
+                {
+                    CONCEPT: {NAMESPACE: 'uniprot', NAME: 'HUS1_HUMAN', IDENTIFIER: 'O60921'},
+                    VARIANTS: [{NAMESPACE: 'go', IDENTIFIER: '0006468', NAME: 'protein phosphorylation'}]
+                },
+                {
+                    CONCEPT: {NAMESPACE: 'uniprot', NAME: 'HUS1_HUMAN'},
+                    VARIANTS: [{NAMESPACE: 'GO', NAME: 'protein phosphorylation'}]
+                },
+            ),
+            (
+                'Fix name in pmod by identifier lookup',
+                {
+                    CONCEPT: {NAMESPACE: 'uniprot', NAME: 'HUS1_HUMAN', IDENTIFIER: 'O60921'},
+                    VARIANTS: [{NAMESPACE: 'go', IDENTIFIER: '0006468', NAME: 'protein phosphorylation'}]
+                },
+                {
+                    CONCEPT: {NAMESPACE: 'uniprot', NAME: 'HUS1_HUMAN'},
+                    VARIANTS: [{NAMESPACE: 'GO', IDENTIFIER: '0006468', NAME: 'WRONG!'}]
+                },
+            ),
+            (
+                'Normalize pmod using default BEL namespace',
+                {
+                    CONCEPT: {NAMESPACE: 'uniprot', NAME: 'HUS1_HUMAN', IDENTIFIER: 'O60921'},
+                    VARIANTS: [{NAMESPACE: 'go', IDENTIFIER: '0006468', NAME: 'protein phosphorylation', KIND: PMOD}]
+                },
+                {
+                    CONCEPT: {NAMESPACE: 'uniprot', NAME: 'HUS1_HUMAN'},
+                    VARIANTS: [{NAMESPACE: 'bel', NAME: 'Ph', KIND: PMOD}]
+                },
+            ),
+            (
+                'Normalize pmod using default BEL namespace - Me (to investigate conflict with gmod)',
+                {
+                    CONCEPT: {NAMESPACE: 'uniprot', NAME: 'HUS1_HUMAN', IDENTIFIER: 'O60921'},
+                    VARIANTS: [{NAMESPACE: 'go', IDENTIFIER: '0006479', NAME: 'protein methylation', KIND: PMOD}]
+                },
+                {
+                    CONCEPT: {NAMESPACE: 'uniprot', NAME: 'HUS1_HUMAN'},
+                    VARIANTS: [{NAMESPACE: 'bel', NAME: 'Me', KIND: PMOD}]
+                },
+            ),
+            (
+                'Normalize gmod using default BEL namespace - Me (to investigate conflict with pmod)',
+                {
+                    CONCEPT: {NAMESPACE: 'hgnc', NAME: 'MAPT', IDENTIFIER: '6893'},
+                    VARIANTS: [{NAMESPACE: 'go', IDENTIFIER: '0006306', NAME: 'DNA methylation', KIND: GMOD}]
+                },
+                {
+                    CONCEPT: {NAMESPACE: 'HGNC', NAME: 'MAPT'},
+                    VARIANTS: [{NAMESPACE: 'bel', NAME: 'Me', KIND: GMOD}]
+                },
+            ),
+        ]
+        for name, expected, result in r:
+            with self.subTest(name=name):
+                _process_node(result)
+                self.assertEqual(expected, result)
 
-    def test_normalize_prefix_synonym(self):
-        """Test that the prefix is normalized by a synonym."""
-        self._test_ground(
-            {NAMESPACE: 'mesh', NAME: 'Neurons', IDENTIFIER: 'D009474'},
-            {NAMESPACE: 'MESHA', NAME: 'Neurons', IDENTIFIER: 'D009474'}
-        )
-
-    def test_add_identifier(self):
-        """Test that the identifier can get looked up."""
-        self._test_ground(
-            {NAMESPACE: 'mesh', NAME: 'Neurons', IDENTIFIER: 'D009474'},
-            {NAMESPACE: 'MESH', NAME: 'Neurons'}
-        )
-
-    def test_add_identifier_fix_wrong_name(self):
-        """Test that the name is replaced if the identifier is already available.."""
-        self._test_ground(
-            {NAMESPACE: 'mesh', NAME: 'Neurons', IDENTIFIER: 'D009474'},
-            {NAMESPACE: 'MESH', NAME: 'Nonsense name!', IDENTIFIER: 'D009474'}
-        )
-
-    def test_remap(self):
-        """Test remapping using FamPlex."""
-        self._test_ground(
-            {NAMESPACE: 'fplx', NAME: 'TAP', IDENTIFIER: 'TAP'},
-            {NAMESPACE: 'SFAM', NAME: 'TAP Family'},
-        )
-        self._test_ground(
-            {NAMESPACE: 'fplx', NAME: 'Gamma_secretase', IDENTIFIER: 'Gamma_secretase'},
-            {NAMESPACE: 'SCOMP', NAME: 'gamma Secretase Complex'},
-        )
+# TODO look up members when checking a named complex, protein family, or pathway
