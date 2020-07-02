37c9de875ea5731263310896deb0fa2bac3ad0e4
aman527 <amanchoudhri@gmail.com>
2020-07-02 11:10:38 -0400

    Move constants back into test file

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -6,10 +6,25 @@ import unittest
 
 import pybel.dsl
 from pybel.schema import is_valid_node
-from tests.schema_constants import (
-    BLANK_ABUNDANCE, FRAGMENT, GENE, GENE_MOD, IDENTIFIER, NAME,
-    NAMESPACE, PROTEIN, PROTEIN_MOD, PROTEIN_SUB
-)
+from pybel.testing.utils import n
+
+NAMESPACE, NAME, IDENTIFIER = n(), n(), n()
+
+BLANK_ABUNDANCE = {
+    "function": "",
+    "variants": []
+}
+
+# Abundances
+PROTEIN = pybel.dsl.Protein(namespace=NAMESPACE, name=NAME, identifier=IDENTIFIER)
+GENE = pybel.dsl.Gene(NAMESPACE, name=NAME, identifier=IDENTIFIER)
+
+# Variants
+GENE_MOD = pybel.dsl.GeneModification(NAME, namespace=NAMESPACE, identifier=IDENTIFIER)
+PROTEIN_SUB = pybel.dsl.ProteinSubstitution('Ala', 1, 'Tyr')
+PROTEIN_MOD = pybel.dsl.ProteinModification(NAME, code='Ala', position=1,
+                                            namespace=NAMESPACE, identifier=IDENTIFIER)
+FRAGMENT = pybel.dsl.Fragment(1, 2)
 
 
 class TestSchema(unittest.TestCase):
