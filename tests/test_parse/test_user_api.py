4ab0a6ad4183f960c42da2d443889ce540712d46
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-07-06 14:05:17 +0200

    Fix bug in parser
    I forgot about a few other places where the parser was adding to the graph

--- tests/test_parse/test_user_api.py
+++ tests/test_parse/test_user_api.py
@@ -11,7 +11,18 @@ class TestParse(unittest.TestCase):
     """Test user API."""
 
     def test_bel_statement(self):
-        x = pybel.parse('p(hgnc:1234) -> p(hgnc:1235)')
+        for bel in [
+            'p(hgnc:1234) -> p(hgnc:1235)',
+            'p(hgnc:1234) hasVariant p(hgnc:1234, pmod(Ph))',
+            'p(hgnc:1) => rxn(reactants(a(chebi:1), a(chebi:2)), products(a(chebi:3), a(chebi:4)))',
+            'r(hgnc:1) pos r(hgnc:2)',
+            'r(hgnc:1) cor r(hgnc:2)',
+            'r(hgnc:1) eq r(ncbigene:5)',
+            'p(hgnc:1) -> (p(hgnc:2) -> p(hgnc:3))',
+            'p(hgnc:1)',
+        ]:
+            with self.subTest(bel=bel):
+                x = pybel.parse(bel)
 
     def test_citation(self):
         x = pybel.parse('SET Citation = pmid:1234')
