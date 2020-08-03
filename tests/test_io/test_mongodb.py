86e6f2976ba6de8ee203e34774c54a196f464c98
aman527 <amanchoudhri@gmail.com>
2020-08-03 10:53:22 -0400

    Debug and rework edge tests for SBEL data format

--- tests/test_io/test_mongodb.py
+++ tests/test_io/test_mongodb.py
@@ -11,7 +11,7 @@ import pymongo
 from pymongo.collection import Collection
 from pymongo.errors import ConnectionFailure
 
-from pybel import BELGraph, to_nodelink
+from pybel import BELGraph
 from pybel.dsl import Entity, ComplexAbundance, Gene, Protein, Rna
 from pybel.io.mongodb import (
     _entity_to_dict,
@@ -21,6 +21,7 @@ from pybel.io.mongodb import (
     get_edges_from_node,
     get_edges_from_criteria
 )
+from pybel.io.sbel import to_sbel
 from pybel.testing.utils import n
 
 g1 = Gene('hgnc', '1')
@@ -38,6 +39,29 @@ ca = ComplexAbundance([p1, g2], name='ca', namespace='hgnc')
 NodeInfo = namedtuple('NodeInfo', 'name identifier variants')
 
 
+def id_info(node: Mapping[str, Any]) -> NodeInfo:
+    """Return the important identifying information for a given node."""
+    # Get the concept entry (where the name / identifier are stored)
+    concept = node['concept']
+    name, identifier, variants = None, None, None
+    # Query based on name, identifier, variants if they are present
+    if 'name' in concept.keys():
+        name = concept['name']
+    if 'identifier' in concept.keys():
+        identifier = concept['identifier']
+    if 'variants' in node.keys():
+        variants = node['variants']
+    return NodeInfo(name=name, identifier=identifier, variants=variants)
+
+
+def _edge_to_dict(edge: dict) -> dict:
+    """Helper function to convert an edge to a dictionary."""
+    new_edge = copy.deepcopy(edge)
+    for key in ('source', 'target'):
+        new_edge[key] = _entity_to_dict(new_edge[key])
+    return new_edge
+
+
 class TestMongoDB(unittest.TestCase):
     """Tests for the MongoDB exporting and querying."""
     def setUp(self):
@@ -46,6 +70,7 @@ class TestMongoDB(unittest.TestCase):
         # Check if there was an error connecting to MongoDB
         try:
             # The ismaster command is cheap and does not require auth.
+            # If it fails, there was a connection failure
             client.admin.command('ismaster')
             # Set up the test mongo database and collection
             TEST_DB = client['test_db']
@@ -56,7 +81,8 @@ class TestMongoDB(unittest.TestCase):
             self.graph.add_increases(ca, r2, citation=n(), evidence=n())
             self.graph.add_increases(p2, p3, citation=n(), evidence=n())
             self.graph.add_decreases(p3, p1, citation=n(), evidence=n())
-            self.links = to_nodelink(self.graph)['links']
+            # The first entry is a dict of annotations, not an edge
+            self.links = to_sbel(self.graph)[1:]
             # Export it to mongo
             self.collection = to_mongodb(self.graph, TEST_DB.name, TEST_COLLECTION.name)
 
@@ -84,6 +110,9 @@ class TestMongoDB(unittest.TestCase):
             assert_fn = self.assertNotEqual
         if not isinstance(edge, dict):
             raise ValueError('Expected edge to be of type dict. Insead found: ', type(edge))
+        # Convert the 'source' and 'target' of each edge to a dict
+        for key in ('source', 'target'):
+            edge[key] = _entity_to_dict(edge[key])
         # Query the collection for the given edge
         match = self.collection.find_one(edge)
         if match is not None:
@@ -104,26 +133,12 @@ class TestMongoDB(unittest.TestCase):
         for link in self.links:
             self.assert_edge(link)
 
-    def id_info(self, node: dict) -> NodeInfo:
-        """Return the important identifying information for a given node."""
-        # Get the concept entry (where the name / identifier are stored)
-        concept = node['concept']
-        name, identifier, variants = None, None, None
-        # Query based on name, identifier, variants if they are present
-        if 'name' in concept.keys():
-            name = concept['name']
-        if 'identifier' in concept.keys():
-            identifier = concept['identifier']
-        if 'variants' in node.keys():
-            variants = node['variants']
-        return NodeInfo(name=name, identifier=identifier, variants=variants)
-
     def test_query_nodes(self):
         """Test that the find_nodes() function correctly finds the desired nodes."""
         for node in self.graph:
             n = _entity_to_dict(node)
             # Get the relevant identifying information for the node
-            n_info = self.id_info(n)
+            n_info = id_info(n)
             # Query the MongoDB based on that info
             matches = find_nodes(
                 self.collection, name=n_info.name, identifier=n_info.identifier, variants=n_info.variants
@@ -133,13 +148,17 @@ class TestMongoDB(unittest.TestCase):
 
             self.assertIn(n, matches)
 
-    def get_true_edges(self, node: dict) -> List[dict]:
+    def _get_true_edges(self, node: Mapping[str, Any]) -> List[dict]:
         """For a given node, return all its edges from self.links"""
-        correct_edges = [edge for edge in self.links
-                         if edge['source'] == node or edge['target'] == node]
+        n = _entity_to_dict(node)
+        correct_edges: List[dict] = []
+        for edge in self.links:
+            dict_edge = _edge_to_dict(edge)
+            if n in (dict_edge['source'], dict_edge['target']):
+                correct_edges.append(dict_edge)
         return correct_edges
 
-    def _edges_from_node(self, node: dict) -> List[dict]:
+    def _edges_from_node(self, node: Mapping[str, Any]) -> List[dict]:
         """Return the matching edges for the given node from get_edges_from_node()."""
         # Get the matches from get_edges_from_node()
         matches_from_node = get_edges_from_node(self.collection, node)
@@ -148,10 +167,10 @@ class TestMongoDB(unittest.TestCase):
             _rm_mongo_keys(match)
         return matches_from_node
 
-    def _edges_from_criteria(self, node: dict) -> Optional[List[dict]]:
+    def _edges_from_criteria(self, node: Mapping[str, Any]) -> List[dict]:
         """Return the matching edges for the given node from get_edges_from_node()."""
         # Get the matches from get_edges_from_criteria()
-        criteria = self.id_info(node)
+        criteria = id_info(node)
         pairs_from_criteria = get_edges_from_criteria(
             self.collection,
             node_name=criteria.name,
@@ -164,9 +183,12 @@ class TestMongoDB(unittest.TestCase):
         for (node_match, edges_match) in pairs_from_criteria:
             _rm_mongo_keys(node_match)
             # If node_match is equal to the given node, let matches_from_criteria equal the returned edges
-            if node_match == node:
+            if node_match == _entity_to_dict(node):
                 matches_from_criteria = edges_match
                 break
+        # Remove the mongo keys from each match
+        for match in matches_from_criteria:
+            _rm_mongo_keys(match)
         return matches_from_criteria
 
     def test_get_edges(self):
@@ -176,7 +198,7 @@ class TestMongoDB(unittest.TestCase):
             n = _entity_to_dict(node)
 
             # Find all the correct edges pointing to and from the current node
-            correct_edges = self.get_true_edges(n)
+            correct_edges = self._get_true_edges(n)
 
             # Get the matching edges from get_edges_from_node()
             matches_from_node = self._edges_from_node(node)
