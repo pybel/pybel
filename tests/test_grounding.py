51bb83570d195601b57f68b73484f6c499e23c28
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-09-25 13:49:19 +0200

    Add mocks for uniprot

--- tests/test_grounding.py
+++ tests/test_grounding.py
@@ -3,6 +3,8 @@
 """Test grounding."""
 
 import unittest
+from unittest import mock
+
 
 from pyobo.mocks import get_mock_id_name_mapping
 
@@ -34,7 +36,28 @@ mock_id_name_mapping = get_mock_id_name_mapping({
 })
 
 
+_mock_mnemonic_data = {
+    'O60921': 'HUS1_HUMAN',
+    'Q99638': 'RAD9A_HUMAN',
+    'O60671': 'RAD1_HUMAN',
+}
+_mock_reverse_mnemonic_data = {v: k for k, v in _mock_mnemonic_data.items()}
+
+
+def _mock_get_mnemonic(identifier, *_, **__):
+    return _mock_mnemonic_data[identifier]
+
+
+mock_get_mnemonic = mock.patch('pybel.grounding.get_mnemonic', side_effect=_mock_get_mnemonic)
+mock_get_id_from_mnemonic = mock.patch(
+    'pybel.grounding.get_id_from_mnemonic',
+    side_effect=_mock_reverse_mnemonic_data.get,
+)
+
+
 @mock_id_name_mapping
+@mock_get_mnemonic
+@mock_get_id_from_mnemonic
 class TestProcessConcept(unittest.TestCase):
     """Test the :func:`_process_concept` function."""
 
@@ -45,63 +68,63 @@ class TestProcessConcept(unittest.TestCase):
         _process_concept(concept=d[CONCEPT], node=d)
         self.assertEqual(expected[CONCEPT], d[CONCEPT])
 
-    def test_normalize_prefix_case(self, _):
+    def test_normalize_prefix_case(self, *_):
         """Test normalizing the prefix to the correct case."""
         self._help(
             {NAMESPACE: 'mesh', NAME: 'Neurons', IDENTIFIER: 'D009474'},
             {NAMESPACE: 'MESH', NAME: 'Neurons', IDENTIFIER: 'D009474'},
         )
 
-    def test_normalize_prefix_synonym(self, _):
+    def test_normalize_prefix_synonym(self, *_):
         """Test normalizing the prefix based on the synonym dictionary."""
         self._help(
             {NAMESPACE: 'mesh', NAME: 'Neurons', IDENTIFIER: 'D009474'},
             {NAMESPACE: 'MESHA', NAME: 'Neurons', IDENTIFIER: 'D009474'},
         )
 
-    def test_lookup_identifier(self, _):
+    def test_lookup_identifier(self, *_):
         """Test look up of the identifier when given the name."""
         self._help(
             {NAMESPACE: 'mesh', NAME: 'Neurons', IDENTIFIER: 'D009474'},
             {NAMESPACE: 'MESH', NAME: 'Neurons'},
         )
 
-    def test_lookup_uniprot_identifier(self, _):
+    def test_lookup_uniprot_identifier(self, *_):
         """Test looking up a uniprot identifier."""
         self._help(
             {NAMESPACE: 'uniprot', NAME: 'HUS1_HUMAN', IDENTIFIER: 'O60921'},
             {NAMESPACE: 'UniProt', NAME: 'HUS1_HUMAN'},
         )
 
-    def test_fix_uniprot_identifier_as_name(self, _):
+    def test_fix_uniprot_identifier_as_name(self, *_):
         """Test lookup of the UniProt identifier when given a UniProt identifier as the name."""
         self._help(
             {NAMESPACE: 'uniprot', NAME: 'HUS1_HUMAN', IDENTIFIER: 'O60921'},
             {NAMESPACE: 'UniProt', NAME: 'O60921'},
         )
 
-    def test_fix_wrong_name(self, _):
+    def test_fix_wrong_name(self, *_):
         """Test overwriting a wrong name (not UniProt)."""
         self._help(
             {NAMESPACE: 'mesh', NAME: 'Neurons', IDENTIFIER: 'D009474'},
             {NAMESPACE: 'MESH', NAME: 'Nonsense name!', IDENTIFIER: 'D009474'},
         )
 
-    def test_fix_wrong_uniprot_name(self, _):
+    def test_fix_wrong_uniprot_name(self, *_):
         """Test overwriting a wrong name (UniProt)."""
         self._help(
             {NAMESPACE: 'uniprot', NAME: 'HUS1_HUMAN', IDENTIFIER: 'O60921'},
             {NAMESPACE: 'UniProt', NAME: 'WRONG!!!!', IDENTIFIER: 'O60921'},
         )
 
-    def test_remap_sfam(self, _):
+    def test_remap_sfam(self, *_):
         """Test remapping SFAM to FPLX."""
         self._help(
             {NAMESPACE: 'fplx', NAME: 'TAP', IDENTIFIER: 'TAP'},
             {NAMESPACE: 'SFAM', NAME: 'TAP Family'},
         )
 
-    def test_remap_scomp(self, _):
+    def test_remap_scomp(self, *_):
         """Test remapping SFAM to FPLX."""
         self._help(
             {NAMESPACE: 'fplx', NAME: 'Gamma_secretase', IDENTIFIER: 'Gamma_secretase'},
@@ -110,6 +133,8 @@ class TestProcessConcept(unittest.TestCase):
 
 
 @mock_id_name_mapping
+@mock_get_mnemonic
+@mock_get_id_from_mnemonic
 class TestGround(unittest.TestCase):
     """Test grounding."""
 
@@ -117,14 +142,14 @@ class TestGround(unittest.TestCase):
         _process_node(result)
         self.assertEqual(expected, result)
 
-    def test_lookup_identifier_member(self, _):
+    def test_lookup_identifier_member(self, *_):
         """Test looking up the identifier of a member by name."""
         self._help(
             {MEMBERS: [{CONCEPT: {NAMESPACE: 'mesh', NAME: 'Neurons', IDENTIFIER: 'D009474'}}]},
             {MEMBERS: [{CONCEPT: {NAMESPACE: 'MESH', NAME: 'Neurons'}}]},
         )
 
-    def test_lookup_identifier_complex(self, _):
+    def test_lookup_identifier_complex(self, *_):
         """Test looking up the identifier of a named complex and its members at the same time."""
         self._help(
             {
@@ -145,28 +170,28 @@ class TestGround(unittest.TestCase):
             },
         )
 
-    def test_lookup_identifier_protein(self, _):
+    def test_lookup_identifier_protein(self, *_):
         """Test looking up the identifier based on a protein's name."""
         self._help(
             {CONCEPT: {NAMESPACE: 'uniprot', NAME: 'HUS1_HUMAN', IDENTIFIER: 'O60921'}},
             {CONCEPT: {NAMESPACE: 'uniprot', NAME: 'HUS1_HUMAN'}},
         )
 
-    def test_lookup_name_protein(self, _):
+    def test_lookup_name_protein(self, *_):
         """Test looking up the name based on a protein's identifier."""
         self._help(
             {CONCEPT: {NAMESPACE: 'uniprot', NAME: 'HUS1_HUMAN', IDENTIFIER: 'O60921'}},
             {CONCEPT: {NAMESPACE: 'uniprot', IDENTIFIER: 'O60921'}},
         )
 
-    def test_fix_name_protein(self, _):
+    def test_fix_name_protein(self, *_):
         """Test fixing a wrong name by overwriting by identifier-based lookup."""
         self._help(
             {CONCEPT: {NAMESPACE: 'uniprot', NAME: 'HUS1_HUMAN', IDENTIFIER: 'O60921'}},
             {CONCEPT: {NAMESPACE: 'uniprot', IDENTIFIER: 'O60921', NAME: 'wrong!!!'}},
         )
 
-    def test_lookup_identifier_pmod(self, _):
+    def test_lookup_identifier_pmod(self, *_):
         """Test looking up a protein modification's identifier by name."""
         self._help(
             {
@@ -189,7 +214,7 @@ class TestGround(unittest.TestCase):
             },
         )
 
-    def test_lookup_name_pmod(self, _):
+    def test_lookup_name_pmod(self, *_):
         """Test looking up a protein modification's name by identifier."""
         self._help(
             {
@@ -212,7 +237,7 @@ class TestGround(unittest.TestCase):
             },
         )
 
-    def test_fix_pmod_name(self, _):
+    def test_fix_pmod_name(self, *_):
         """Test fixing a wrong name in a pmod."""
         self._help(
             {
@@ -235,7 +260,7 @@ class TestGround(unittest.TestCase):
             },
         )
 
-    def test_normalize_pmod_default(self, _):
+    def test_normalize_pmod_default(self, *_):
         """Test normalizing a pmod using the default bel namespace."""
         self._help(
             {
@@ -258,7 +283,7 @@ class TestGround(unittest.TestCase):
             },
         )
 
-    def test_normalize_pmod_default_methylation(self, _):
+    def test_normalize_pmod_default_methylation(self, *_):
         """Test normalizing the default namespace's Me entry because of conflict with gmods."""
         self._help(
             {
@@ -281,7 +306,7 @@ class TestGround(unittest.TestCase):
             },
         )
 
-    def normalize_gmod_default_methylation(self, _):
+    def normalize_gmod_default_methylation(self, *_):
         """Test normalizing the default namespace's Me entry because of conflict with pmods."""
         self._help(
             {
@@ -309,42 +334,42 @@ class TestAnnotations(unittest.TestCase):
         _process_annotations(data)
         self.assertEqual(expected_data, data)
 
-    def test_lookup_by_identifier(self, _):
+    def test_lookup_by_identifier(self, *_):
         """Test lookup by identifier."""
         self._help(
             {'Disease': [Entity(namespace='mesh', identifier='D010300', name='Parkinson Disease')]},
             {'Disease': [Entity(namespace='mesh', identifier='D010300')]},
         )
 
-    def test_lookup_by_name(self, _):
+    def test_lookup_by_name(self, *_):
         """Test lookup by name."""
         self._help(
             {'Disease': [Entity(namespace='mesh', identifier='D010300', name='Parkinson Disease')]},
             {'Disease': [Entity(namespace='mesh', name='Parkinson Disease')]},
         )
 
-    def test_lookup_by_name_as_identifier(self, _):
+    def test_lookup_by_name_as_identifier(self, *_):
         """Test lookup by name if it's accidentally in the identifier slot."""
         self._help(
             {'Disease': [Entity(namespace='mesh', identifier='D010300', name='Parkinson Disease')]},
             {'Disease': [Entity(namespace='mesh', identifier='Parkinson Disease')]},
         )
 
-    def test_upgrade_category(self, _):
+    def test_upgrade_category(self, *_):
         """Test upgrading the category."""
         self._help(
             {'Disease': [Entity(namespace='mesh', identifier='D010300', name='Parkinson Disease')]},
             {'MeSHDisease': [Entity(namespace='mesh', identifier='D010300', name='Parkinson Disease')]},
         )
 
-    def test_upgrade_category_and_namespace(self, _):
+    def test_upgrade_category_and_namespace(self, *_):
         """Test upgrading the category and the namespace simultaneously."""
         self._help(
             {'Disease': [Entity(namespace='mesh', identifier='D010300', name='Parkinson Disease')]},
             {'MeSHDisease': [Entity(namespace='MeSHDisease', identifier='D010300', name='Parkinson Disease')]},
         )
 
-    def test_upgrade_with_name_as_identifier(self, _):
+    def test_upgrade_with_name_as_identifier(self, *_):
         """Test upgrading MeSH disease, MeSH anatomy, and Species tags and lookup by name, in the identifiers space."""
         self._help(
             {  # Expected
@@ -359,7 +384,7 @@ class TestAnnotations(unittest.TestCase):
             },
         )
 
-    def test_upgrade_by_identifier(self, _):
+    def test_upgrade_by_identifier(self, *_):
         """Test upgrading and lookup by identifier."""
         self._help(
             {  # Expected
@@ -374,7 +399,7 @@ class TestAnnotations(unittest.TestCase):
             },
         )
 
-    def test_upgrade_by_name(self, _):
+    def test_upgrade_by_name(self, *_):
         """Test upgrading and lookup by name."""
         self._help(
             {  # Expected
@@ -389,7 +414,7 @@ class TestAnnotations(unittest.TestCase):
             },
         )
 
-    def test_unmappable_category(self, _):
+    def test_unmappable_category(self, *_):
         """Test when the category can't be mapped."""
         self._help(
             {  # Expected
@@ -400,7 +425,7 @@ class TestAnnotations(unittest.TestCase):
             },
         )
 
-    def test_unmappable_identifier(self, _):
+    def test_unmappable_identifier(self, *_):
         """Test when the identifier can not be resolved."""
         self._help(
             {  # Expected
@@ -411,7 +436,7 @@ class TestAnnotations(unittest.TestCase):
             },
         )
 
-    def test_unmappable_name(self, _):
+    def test_unmappable_name(self, *_):
         """Test when the identifier can not be looked up by name."""
         self._help(
             {  # Expected
