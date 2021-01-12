b6c49cf1f9782553d4c41653e59bf652db13fb4d
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-01-12 15:33:46 +0100

    Update graph.py
    Closes #479

--- src/pybel/struct/graph.py
+++ src/pybel/struct/graph.py
@@ -510,7 +510,7 @@ class BELGraph(nx.MultiDiGraph):
         """
         if 'subject_modifier' in attr:
             warnings.warn('subject_modifier has been renamed to source_modifier')
-            source_modifier = attr.pop('source_modifier')
+            source_modifier = attr.pop('subject_modifier')
         if 'object_modifier' in attr:
             warnings.warn('object_modifier has been renamed to target_modifier')
             target_modifier = attr.pop('object_modifier')
